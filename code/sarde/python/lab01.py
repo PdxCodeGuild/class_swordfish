e55fbfaa14a0db6a095dd4cfe8a68c9e7189d02b
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-09-07 19:32:38 -0400

    Lab01 in still in progress

---
+++ code/sarde/python/lab01.py
@@ -0,0 +1,13 @@
+# Lab 1: Unit Converter
+'''
+Version 1
+-Ask the user for the number of feet
+-print equivalent distance in meters
+- 1 ft is 0.3048 m.
+-output = meters = multiply input distance by 0.3048
+'''
+units = {
+    'feet': 0.3048,
+}
+# ask user for number of feet
+feet = input('What is the distance in feet?: ')
