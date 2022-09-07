c8fcf46cab85769f36f820314aabc8f35ffc63b1
Jose Cortes <jcortes8989@gmail.com>
2022-09-07 15:53:42 -0700

    Version 2

--- Python_Labs/lab01.py
+++ Python_Labs/lab01.py
@@ -1,13 +1,37 @@
+# conversion_values = { 'feet': 0.3048,
+# 'miles': 1609.34, 
+# 'meters': 1, 
+# 'kilometers': 1000
+# }
+
+# user_measurement = input('Please enter a number to convert to meters: ')
+
+# user_measurement = int(user_measurement)
+
+# converted_measurment = user_measurement * conversion_values['feet']
+
+# print(converted_measurment)
+
+
+
+
+# units = {'mi': 'miles',
+# 'ft': 'feet',
+# 'm': 'meter',
+# 'km': 'kilometers'}
+
 conversion_values = { 'feet': 0.3048,
 'miles': 1609.34, 
 'meters': 1, 
 'kilometers': 1000
 }
 
-user_measurement = input('Please enter a number to convert to meters: ')
+distance = input('What is the distance: ')
 
-user_measurement = int(user_measurement)
+distance = int(distance)
 
-converted_measurment = user_measurement * conversion_values['feet']
+user_units = input('What are the units? (feet/miles/meters/kilometers): ')
 
-print(converted_measurment)
+chosen_conversion = distance * conversion_values[user_units]
+
+print(f"{distance} {user_units} is", round(chosen_conversion, 2), "m")
