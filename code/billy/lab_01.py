311f22f2d37515283fd923ab49ba49648f95cd50
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-09-07 16:34:50 -0700

    Started lab 1

---
+++ code/billy/lab_01.py
@@ -0,0 +1,31 @@
+'''
+program: lab_01
+author: billy frick
+date: 7 september 2022
+
+function: this program will allow the user to enter the distance of a unit
+and have it converted into meters.
+
+'''
+
+unit_converter = {
+
+    'ft': 0.3048,
+    'mi': 1609.34,
+    'm': 1,
+    'km': 1000,
+    'y': 0.9144,
+    'in': 0.0254
+}
+
+distance = input("\n> what is the distance? ")
+
+distance = int(distance)
+
+unit = input("\n> what are the input units? ")
+
+output_unit = input("\n> what are the output units? ")
+
+output_distance = ({distance * unit_converter[unit]})
+
+print(f"> {distance} {unit} is {output_distance / unit_converter[output_unit]}")
