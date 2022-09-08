c64e45efa3e5ce43831b204ff94ed279e4aedc28
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2022-09-08 19:34:43 -0400

    lab 02 in work

---
+++ code/rachel/Python/lab02.py
@@ -0,0 +1,15 @@
+numbers = {
+    '1': one,
+    '2': two,
+    '3': three,
+    '4': four,
+    '5': five,
+    '6': six,
+    '7': seven,
+    '8': eight,
+    '9': nine
+}
+
+arabic = input("Enter a number: ")
+
+print(f{arabic} % 10)
