c9ef5d57ab0e6e695a7cea4185e604c9c7b71920
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-09-08 19:15:59 -0400

    Lab02 still in prrogress

--- code/sarde/python/lab02.py
+++ code/sarde/python/lab02.py
@@ -5,10 +5,30 @@ Lab 2: Number to Phrase
 '''
 # converting from an integer to a string????
 # use the digit, as a key for a dict of digit:phrase pairs
-digits = {
+single_suffix = {
     6: 'six',
-    7: 'seven'
+    7: 'seven',
+    8: 'eight',
+    9: 'nine'
 }
-print(digits[6]+''+'-'+digits[7])
-print('Hello World!')
-print('GoodBye World!')
+tens_suffix = {
+    8: 'eighty',
+    9: 'ninety'
+}
+# print(digits[6]+''+'-'+digits[7])
+
+input_number = 97
+
+
+tens_digit = input_number // 10
+ones_digit = input_number % 10
+#print(tens_digit, ones_digit)
+if tens_digit >= 20:
+    # do suff for 20 and above
+    pass
+elif tens_digit >= 10 and tens_digit <= 19:
+    # do stuff for 10-19
+    pass
+else:
+    # do stuff for 0-9
+    pass
