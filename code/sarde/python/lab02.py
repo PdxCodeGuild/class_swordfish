42f8b3ef8d04e9f2e44c6b7f8a68069c14b94a0b
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-09-08 18:40:27 -0400

    Lab02 in progress

---
+++ code/sarde/python/lab02.py
@@ -0,0 +1,13 @@
+'''
+Lab 2: Number to Phrase
+-convert a given number into its english representation
+-Handles numbers from 0-99
+'''
+# converting from an integer to a string????
+# use the digit, as a key for a dict of digit:phrase pairs
+digits = {
+    6: 'six',
+    7: 'seven'
+}
+print(digits[6]+''+'-'+digits[7])
+print('Hello World!')
