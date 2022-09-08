0456f864cfde64158fc980e0a77de7bf4d76de3c
VanguardAWS <ethanturner93@hotmail.com>
2022-09-08 16:13:59 -0500

    lab02 version 3

--- code/ethan/python/lab02.py
+++ code/ethan/python/lab02.py
@@ -2,43 +2,10 @@
 
 # Version 1 & 2
 
-# numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
-# 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 
-# tens = ['void', 'void', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-
-# num = input('Type a number between 1-999. ')
-# num = int(num)
-
-# if num < 20:
-#     print(numbers[num])
-# elif num > 20 and num < 100:
-#     num = str(num)
-#     num = list(num)
-#     digit_one = num[0]
-#     digit_one = int(digit_one)
-#     digit_two = num[1]
-#     digit_two = int(digit_two)
-#     print(tens[digit_one] + numbers[digit_two])
-# elif num > 99 and num < 1000:
-#     num = str(num)
-#     num = list(num)
-#     digit_one = num[0]
-#     digit_one = int(digit_one)
-#     digit_two = num[1]
-#     digit_two = int(digit_two)
-#     digit_three = num[2]
-#     digit_three = int(digit_three)
-#     print(f"{numbers[digit_one]}hundred and {tens[digit_two] + numbers[digit_three]}")
-
-# Version 3
-
-numbers = ['zero', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 
-'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX']
-
-tens = ['void', 'void', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
-
-hundreds = ['void', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+tens = ['void', 'void', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
 num = input('Type a number between 1-999. ')
 num = int(num)
@@ -62,4 +29,4 @@ elif num > 99 and num < 1000:
     digit_two = int(digit_two)
     digit_three = num[2]
     digit_three = int(digit_three)
-    print(f"{hundreds[digit_one] + tens[digit_two] + numbers[digit_three]}")
+    print(f"{numbers[digit_one]}hundred and {tens[digit_two] + numbers[digit_three]}")
