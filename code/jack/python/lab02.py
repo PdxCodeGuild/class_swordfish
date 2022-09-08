1ba736acfa17226681e22a43e4ce97e7b490683e
Jack Amoss <jackamoss@Jacks-MacBook-Pro.local>
2022-09-08 16:12:51 -0700

    revised to be more concise

--- code/jack/python/lab02.py
+++ code/jack/python/lab02.py
@@ -1,4 +1,5 @@
 #Lab 2: Number to Phrase
+#includes both v2 and v3
 
 number_input = input('enter integer 0-999: ') #get input from user as string for digit extraction
 number_output = '' #string is initiated to prepare for concatenation
@@ -20,45 +21,31 @@ output_type = input('Which output type do you want? (roman or phrase) ').lower()
 if output_type == 'phrase':
 
     #phrase options
-    ones_phrase = ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
-    tens_phrase = ('twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety')
+    ones_phrase = ('', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+    tens_phrase = ('', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety')
     other_phrase = ('ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen')
-
-    #function to turn 1-9 into phrase
-    def ones_to_phrase(x):
-        for phrase in ones_phrase:
-            if x - 1 == ones_phrase.index(phrase):
-                return(phrase)
-
-    #function to turn 2x-9x into phrase
-    def tens_to_phrase(x):
-        for phrase in tens_phrase:
-            if x - 2 == tens_phrase.index(phrase):
-                return(phrase)
 
     #begin concatenating phrase
 
     #concatenates how many hundreds if not 0
     if hundreds_digit != 0:
-        number_output += ones_to_phrase(hundreds_digit) + ' hundred'
+        number_output += ones_phrase[hundreds_digit] + ' hundred'
         if tens_digit != 0 or ones_digit != 0: #'and' only needed if number phrase follows hundred
             number_output += ' and '
 
     #concatenates how many tens if not 0 or teens (greater than 1)
     if tens_digit > 1:
-        number_output += tens_to_phrase(tens_digit)
+        number_output += tens_phrase[tens_digit]
         if ones_digit != 0:
             number_output += '-'
 
     #concatenates if number includes teens
     if tens_digit == 1:
-        for phrase in other_phrase:
-            if ones_digit == other_phrase.index(phrase):
-                number_output += phrase
+        number_output += other_phrase[ones_digit]
 
     #concatenates if ones digit is not 0, and if not teens
     elif ones_digit > 0:
-        number_output += ones_to_phrase(ones_digit)
+        number_output += ones_phrase[ones_digit]
 
     #assigns 'zero' phrase if number is 0
     if number_input == 0:
@@ -72,16 +59,10 @@ elif output_type == 'roman':
     tens_roman = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
     hundreds_roman = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
 
-    #function to take digit and corresponding numeral options and returns numeral
-    def concat_roman(digit, roman_list):
-        for x in roman_list:
-            if digit == roman_list.index(x):
-                return x
-
-    #function is called for each of the 3 digits
-    number_output += concat_roman(hundreds_digit, hundreds_roman)
-    number_output += concat_roman(tens_digit, tens_roman)
-    number_output += concat_roman(ones_digit, ones_roman)
+    #number_output is concatenated with each digit's respective numeral
+    number_output += hundreds_roman[hundreds_digit]
+    number_output += tens_roman[tens_digit]
+    number_output += ones_roman[ones_digit]
     if number_input == 0: #special case for 0 since there is no roman numeral for 0
         number_output = 'Error: no roman numeral for 0' 
 
