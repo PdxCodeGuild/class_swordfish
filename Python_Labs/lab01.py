c504dd6db9e4504d9c51d33029892ec28fb07110
Jose Cortes <jcortes8989@gmail.com>
2022-09-08 14:07:47 -0700

    Version 4

--- Python_Labs/lab01.py
+++ Python_Labs/lab01.py
@@ -20,6 +20,25 @@
 # 'm': 'meter',
 # 'km': 'kilometers'}
 
+# conversion_values = { 'feet': 0.3048,
+# 'miles': 1609.34, 
+# 'meters': 1, 
+# 'kilometers': 1000,
+# 'yards': 0.9144,
+# 'inches': 0.0254 
+# }
+
+# distance = input('What is the distance: ')
+
+# distance = int(distance)
+
+# user_units = input('What are the units? (feet/miles/meters/kilometers/yards/inches): ')
+
+# chosen_conversion = distance * conversion_values[user_units]
+
+# print(f"{distance} {user_units} is", round(chosen_conversion, 2), "m")
+
+
 conversion_values = { 'feet': 0.3048,
 'miles': 1609.34, 
 'meters': 1, 
@@ -28,18 +47,44 @@ conversion_values = { 'feet': 0.3048,
 'inches': 0.0254 
 }
 
-distance = input('What is the distance: ')
+conversion_values_meters = {'feet': 3.2808399,
+'miles': .00621,
+'kilometers': .001,
+'yards': 1.0936133,
+'inches': 39.3700787
+}
 
-distance = int(distance)
+user_distance = input('What is the distance?: ')
+user_distance = int(user_distance)
 
-user_units = input('What are the units? (feet/miles/meters/kilometers/yards/inches): ')
+user_units = input ('What is the units of the original distance?: ')
+desired_units = input('What do you want the new unit of measurment to be?: ')
 
-chosen_conversion = distance * conversion_values[user_units]
+if user_units == 'feet':
+    userunits_meters = user_distance * conversion_values['feet']
+elif user_units == 'miles':
+    userunits_meters = user_distance * conversion_values['miles']
+elif user_units == 'kilometers':
+    userunits_meters = user_distance * conversion_values['kilometers']
+elif user_units == 'yards':
+    userunits_meters = user_distance * conversion_values['yards']
+elif user_units == 'inches':
+    userunits_meters = user_distance * conversion_values['inches']
 
-print(f"{distance} {user_units} is", round(chosen_conversion, 2), "m")
+final_converted_units = userunits_meters * conversion_values_meters[desired_units]
+print ('Your new units are ' + str(final_converted_units) + str(desired_units))
 
-new_units = input('What units would you like to convert to?(feet/miles/meters/kilometers/yards/inches): ')
 
-new_conversion = conversion_values[new_units] * chosen_conversion
 
-print('Your new conversion is', new_conversion)
+# print(new_units + desired_units)
+
+
+
+ 
+    
+
+# new_conversion = conversion_values[new_units] * chosen_conversion
+
+# new_conversion = str(new_conversion)
+
+# print('Your new conversion is', new_conversion + new_units)
