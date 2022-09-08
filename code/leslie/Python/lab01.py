3cb318febad23cb184c4d2f8e724880c75cf738d
Leslie <leslie.l.naylor@gmail.com>
2022-09-08 16:36:21 -0400

    version 3

--- code/leslie/Python/lab01.py
+++ code/leslie/Python/lab01.py
@@ -1,9 +1,11 @@
+#Version 1
 '''feet = int(input("What is the distance in feet? "))
 meters = feet * 0.3048
 print(feet,"feet is",float(meters), "meters")
 '''
 
-distance = int(input("What is the distance?"))
+#Version 2
+'''distance = int(input("What is the distance?"))
 units = input("What are the units?")
 if units == "meters":
     meters = distance
@@ -13,9 +15,23 @@ elif units == "miles":
     meters = distance / 1609
 elif units == "kilometers":
     meters = distance * 1000
+print(int(distance),units,"equals",meters,"meters")'''
 
+#Version 3
+distance = int(input("What is the distance? "))
+units = input("What are the units? ")
+if units == "meters":
+    meters = distance
+elif units == "feet":
+    meters = distance * .3048
+elif units == "miles":
+    meters = distance * 1609
+elif units == "kilometers":
+    meters = distance * 1000
+elif units == "yards":
+    meters = distance / 1.094
+elif units == "inches":
+    meters = distance / 39.37
 
 print(int(distance),units,"equals",meters,"meters")
-
-
 
