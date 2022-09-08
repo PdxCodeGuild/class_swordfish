fc33a5908e88d2bdd1baa1125ca7b4e28427ebfc
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2022-09-07 22:42:10 -0400

    lab 1 in work

---
+++ code/rachel/Python/lab01.py
@@ -0,0 +1,12 @@
+#Version 1
+
+meter = 0.3048
+
+unit = input("Enter the number of feet: ")
+
+unit = float(unit)
+meter = float(meter)
+
+total_meters = unit * meter
+
+print(unit + "feet is equal to " + total_meters + "meters.")
