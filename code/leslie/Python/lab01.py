82796b3518f16c289e25b279c5e1ed5691f6120e
Leslie <leslie.l.naylor@gmail.com>
2022-09-08 17:40:02 -0400

    Version 4

--- code/leslie/Python/lab01.py
+++ code/leslie/Python/lab01.py
@@ -18,7 +18,7 @@ elif units == "kilometers":
 print(int(distance),units,"equals",meters,"meters")'''
 
 #Version 3
-distance = int(input("What is the distance? "))
+'''distance = int(input("What is the distance? "))
 units = input("What are the units? ")
 if units == "meters":
     meters = distance
@@ -34,4 +34,30 @@ elif units == "inches":
     meters = distance / 39.37
 
 print(int(distance),units,"equals",meters,"meters")
+'''
+#Version 4
+distance = int(input("What is the distance? "))
+from_units= input("What are the units? ")
+to_units = input("What unit are you converting to? ")
+if from_units == "meters":
+    meters = distance
+elif from_units == "feet":
+    meters = distance * .3048
+elif from_units == "miles":
+    meters = distance * 1609
+elif from_units == "kilometers":
+    meters = distance * 1000
+    
+if to_units == "meters":
+    converted_distance = meters
+elif to_units == "feet":
+    converted_distance = meters / .0348
+elif to_units == "miles":
+    converted_distance = meters / 1609.34
+elif to_units == "kilometers":
+    converted_distance = meters / 1000
+   
+print(int(distance),from_units,"equals",converted_distance,to_units)  
+
+
 
