e041e5cf46ceba6e6cc9f79a5f3165861dab39af
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-09-09 15:31:22 -0700

    Final draft of lab 2

--- code/billy/lab_02.py
+++ code/billy/lab_02.py
@@ -7,34 +7,6 @@
 
 # '''
 
-
-# list = {
-
-#     'tens': ["zero", "one", "twenty-", "thirty-", "fourty-", "fifty-", "sixty-", "seventy-", "eighty-", "ninety-"],
-
-#     'teens':["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
-
-#     'ones': ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",]
-
-# }
-
-# x = input("\nWelcome! Please enter a number between 0 and 99 ")
-
-# x = int(x)
-
-# tens_digit = x//10
-
-# ones_digit = x%10
-
-# if tens_digit == 1: 
-#     print(f"{list['teens'][ones_digit]}")
-
-# elif tens_digit == 0:
-#     print(f"{list['ones'][ones_digit]}")
-
-# else:
-#     print(f"{list['tens'][tens_digit]}{list['ones'][ones_digit]}")
-
 list = {
 
     'tens': ["zero", "one", "twenty-", "thirty-", "fourty-", "fifty-", "sixty-", "seventy-", "eighty-", "ninety-"],
@@ -45,10 +17,13 @@ list = {
 
 }
 
+#grabs users input to determine value of x
 x = input("\nWelcome! Please enter a number between 0 and 999 ")
 
+#converts x into an integer
 x = int(x)
 
+#This will be executed if x is less than 100. The value of x will then be converted to the English phrase.
 if x < 100:
 
     list = {
@@ -60,10 +35,11 @@ if x < 100:
     'ones': ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",]
 
 }
+    #floor division to get the digit in the tens place.
+    tens_digit = x // 10
 
-    tens_digit = x//10
-
-    ones_digit = x%10
+    #modulus will to grab the single digit left over.
+    ones_digit = x % 10
 
     if tens_digit == 1: 
         print(f"{list['teens'][ones_digit]}")
@@ -76,18 +52,41 @@ if x < 100:
 
 elif x >= 100 and x <= 999:
 
-    hundreds_digits = [-1]
+    hundreds_digit = x // 100
 
-    hundreds_list = {
+    tens_digit = x  % 100 // 10
 
-    'hundreds': ["zero", "one-hundred", "two-hundred", "three-hundred", "four-hundred", "five-hundred", "six-hundred", "seven-hundred", "eight-hundred", "nine-hundred"],
+    ones_digit = x % 10
 
-    'tens': ["zero", "one", "twenty-", "thirty-", "fourty-", "fifty-", "sixty-", "seventy-", "eighty-", "ninety-"],
+    hundreds_dict = {
 
-    'teens':["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
-
-    'ones': ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",]
+    'hundreds': 
+            ["zero", 
+            "one-hundred-", 
+            "two-hundred-", 
+            "three-hundred-", 
+            "four-hundred-", 
+            "five-hundred-", 
+            "six-hundred-", 
+            "seven-hundred-", 
+            "eight-hundred-", 
+            "nine-hundred-"],
     
     }
 
-    print(f"{list['hundreds'][hundreds_digits]}")
+    hundreds_dict['tens'] = list['tens']
+
+    hundreds_dict['teens'] = list['teens']
+
+    hundreds_dict['ones'] = list['ones']
+
+    hundreds_word = hundreds_dict['hundreds'][hundreds_digit]
+
+    if tens_digit == 1: 
+        print(f"{hundreds_word}{hundreds_dict['teens'][ones_digit]}")
+
+    elif tens_digit == 0:
+        print(f"{hundreds_word}{hundreds_dict['ones'][ones_digit]}")
+
+    else:
+        print(f"{hundreds_word}{hundreds_dict['tens'][tens_digit]}{list['ones'][ones_digit]}")
