650988dbbfd41b0e81408246e395af1138ef3c64
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-09-09 17:12:48 -0400

    Lab02 almost finished

--- code/sarde/python/lab02.py
+++ code/sarde/python/lab02.py
@@ -5,30 +5,62 @@ Lab 2: Number to Phrase
 '''
 # converting from an integer to a string????
 # use the digit, as a key for a dict of digit:phrase pairs
-single_suffix = {
+from colorsys import ONE_SIXTH
+
+
+ones_suffix = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
     6: 'six',
     7: 'seven',
     8: 'eight',
     9: 'nine'
 }
+# 20-90
 tens_suffix = {
+    2: 'twenty',
+    3: 'thirty',
+    4: 'fourty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
     8: 'eighty',
     9: 'ninety'
 }
-# print(digits[6]+''+'-'+digits[7])
+# 10-19
+teens_suffix = {
+    0: 'ten',
+    1: 'eleven',
+    2: 'twelve',
+    3: 'thirteen',
+    4: 'fourteen',
+    5: 'fifteen',
+    6: 'sixteen',
+    7: 'seventeen',
+    8: 'eighteen',
+    9: 'nineteen'
+}
 
-input_number = 97
+input_number = 1
 
 
-tens_digit = input_number // 10
-ones_digit = input_number % 10
-#print(tens_digit, ones_digit)
-if tens_digit >= 20:
-    # do suff for 20 and above
-    pass
-elif tens_digit >= 10 and tens_digit <= 19:
+tens_digit = input_number // 10  # prints 9
+ones_digit = input_number % 10  # prints 7
+
+
+if tens_digit > 2:
+    # do suff for 2 and above
+    print(f'{tens_suffix[tens_digit]}-{ones_suffix[ones_digit]}')
+
+
+elif ones_digit >= 0 and ones_digit <= 9:
     # do stuff for 10-19
-    pass
+    print(f'{teens_suffix[ones_digit]}')
+
 else:
     # do stuff for 0-9
-    pass
+    print(f'{ones_suffix[tens_digit]}')
