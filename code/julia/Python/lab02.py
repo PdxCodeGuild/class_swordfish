6870bd0a02a76cfb593dac1ac15a1c3bf1602042
Julia Chobu <juliachobu@Julias-MacBook-Air.local>
2022-09-12 16:07:03 -0700

    labs

--- code/julia/Python/lab02.py
+++ code/julia/Python/lab02.py
@@ -1,20 +1,35 @@
 #Number To Phrase
  
-x = int(input("Enter a number between 0 and 99: "))
+x = int(input("Enter a number between 0 and 999: "))
 
 tens_digit = x//10
 ones_digit = x%10
 
 tens = ["zero", "one", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninty"]
-ones =["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-teens =["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen"]
+ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen"]
+hundreds = ["zero", "one-hundred", "two-hundred", "three-hundred", "four-hundred", "five-hundred", 
+"six-hundred", "seven-hundred", "eight-hundred", "nine-hundred", "nine-hundred and ninty-nine"]
 
-if tens_digit < 0:
+if x < 100:
+    if tens_digit == 0:
+        print(f"{ones[ones_digit]} ")
 
- print(f"{tens[tens_digit]} {ones[ones_digit]} ")
+    elif tens_digit == 1:
+        print(f"{teens[ones_digit]} ")
 
-elif tens_digit == 1:
-    print(f"{teens[ones_digit]}")
+    elif tens_digit > 1:
+        print(f"{tens[tens_digit]} {ones[ones_digit]} ")
 
 else:
-    print(f"{ones[ones_digit]}")   
+    tens_digit = str(x)[1]
+    hundreds_digit = str(x)[0]
+
+    if int(tens_digit) == 0:
+        print(f"{hundreds[int(hundreds_digit)]} {ones[ones_digit]} ")
+
+    elif int(tens_digit) == 1:
+        print(f"{hundreds[int(hundreds_digit)]} {teens[ones_digit]} ")
+
+    elif int(tens_digit) > 1:
+        print(f"{hundreds[int(hundreds_digit)]} {tens[int(tens_digit)]} {ones[ones_digit]} ")
