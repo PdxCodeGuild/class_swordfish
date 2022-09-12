de49550051de5af3711283991063d66541fd061e
Alden Addor <alden@aldens-air.lan>
2022-09-12 12:45:05 -0600

    Lab 4 submit.

--- code/alden/python/lab04.py
+++ code/alden/python/lab04.py
@@ -6,11 +6,11 @@ def pick6():                                # Function producing 6 random number
         random_num = random.randint(1, 10)
         pick6.append(random_num)
     return pick6
-win_tik = pick6()                           # Use the function to generate a winning ticket.
+win_tik = pick6()     # Use the function to generate the winning numbers.
 
-def num_match():                            # Figures out number of matchs in a ticket
+def num_match():      # Figures out number of matching numbers on each ticket to the winning numbers.
     winning = 0
-    if tik_num[0] == win_tik[0]:
+    if tik_num[0] == win_tik[0]:        
         winning += 1
     elif tik_num[1] == win_tik[1]:
         winning += 1
@@ -24,34 +24,35 @@ def num_match():                            # Figures out number of matchs in a 
         winning += 1
     return winning
 
-balance = 0                                 # My money balance
-num_tiks = 0                                # Place holder for the number of tickets
-winning_tik = 0
+balance = 0        # My money balance
+num_tiks = 0       # Place holder for the number of tickets
+winning_tik = 0    # Place holder for number of winning tickets
+earnings = 0       # Place holder for earnings from winning tickets
 
-while num_tiks < 100000:                    # While loop to gerate tickets and take $2 per ticket
-    tik_num = pick6()                       # The 6 numbers on the ticket.
-    balance -= 2                            # Takes the money every ticket
-    num_tiks += 1                           # Creates a new ticket.
+while num_tiks < 100000:        # While loop to gerate tickets and take $2 per ticket
+    tik_num = pick6()           # The 6 numbers on the ticket.
+    balance -= 2                # Takes the money every ticket
+    num_tiks += 1               # Creates a new ticket.
     num_match()
     if num_match() > 0:         # Figures out the number of winning tickets
         winning_tik += 1
-    if num_match()==1:          # Adds money to balance for number of matching numbers
-        balance += 4
+    if num_match()==1:          # Adds money to earnings for number of matching numbers
+        earnings += 4
     elif num_match()==2:
-        balance += 7
+        earnings += 7
     elif num_match()==3:
-        balance += 100
+        earnings += 100
     elif num_match()==4:
-        balance += 50000
+        earnings += 50000
     elif num_match()==5:
-        balance += 1000000
+        earnings += 1000000
     elif num_match()==6:
-        balance += 25000000
-    
-    
-    # print(num_match())
-    # print(tik_num)
+        earnings += 25000000
 
-# print(win_tik)
-print(f'{winning_tik} winning tickets')     # Prints number of winning tickets
-print(f'${balance}')                        # Prints final balance
+
+roi = (earnings - balance)/balance      # Calulates a return on investment percentage.  
+
+print(f'You have {winning_tik} winning tickets out of 100000')     # Prints number of winning tickets
+
+        # Prints out the end statement.
+print(f'You have won ${earnings}!!\n But, your ROI is ${roi}%.\n That means your new balance is ${earnings + balance}.')
