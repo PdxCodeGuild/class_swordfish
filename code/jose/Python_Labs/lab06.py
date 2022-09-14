216a5b6b44c3adc3ce5c2fb39ac029c6e721df7b
Jose Cortes <jcortes8989@gmail.com>
2022-09-14 16:31:38 -0700

    fixed lab 2

--- code/jose/Python_Labs/lab06.py
+++ code/jose/Python_Labs/lab06.py
@@ -5,24 +5,36 @@ letters_list = list(letters)
 
 # user_string = input('Please type in what you want encoded: ')
 # user_string = (list(user_string))
-user_string= 'surface'
+user_string= input("Enter a word to encode it: ")
 user_list = list(user_string)
 
 index_numbers = []
 
-def finding_indexes_in_list(working_list, new_list):
-    for i in range(len(working_list)):
+def find_index(a):
+    """finds the index number in a corresponding list"""
+    for i in enumerate(index_numbers):
+        index_numbers.append(i)
+        print(index_numbers)
+        return index_numbers
 
-        # print(working_list[i])
-        for j in range(len(new_list)):
-            # print(new_list[j])                                           
-            if new_list[j] == working_list[i]:
-                the_letter = working_list[i]
-                the_index = j
-                return index_numbers
-                encoded_string = letters[the_index]
+print(find_index(user_string))
+
+
+
+# def finding_indexes_in_list(working_list, new_list):
+#     for i in range(len(working_list)):
+#          # print(working_list[i])
+#         for j in range(len(new_list)):
+#             # print(new_list[j])                                           
+#             if new_list[j] == working_list[i]:
+#                 the_letter = working_list[i]
+#                 the_index = j
+#                 return index_numbers
+#                 encoded_string = letters[the_index]
          
-                print(the_index, the_letter, encoded_string)
+#                 print(the_index, the_letter, encoded_string)
+
+# print(finding_indexes_in_list(user_string, letters))
 
 
 
@@ -34,7 +46,7 @@ def finding_indexes_in_list(working_list, new_list):
     #         matched_indicies.append()
     #         return matched_indicies
 
-finding_indexes_in_list(user_list, letters_list)
+# finding_indexes_in_list(user_list, letters_list)
 
 # print(finding_indexes_in_list(user_string, letters))
 
