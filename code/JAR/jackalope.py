23902234a42af1a8eb71d162c59396d68dd2ae67
Alden Addor <alden@aldens-air.lan>
2022-09-14 12:25:04 -0600

    added to code

--- code/JAR/jackalope.py
+++ code/JAR/jackalope.py
@@ -3,12 +3,13 @@
 population = [0, 0]
 year = 0
 
-while len(population) < 5:
-    for i in population:
+while len(population) < 15:
+    for i in range(len(population)):
         population[i] += 1
-        # if population[i] == 10:
-        #     population.pop(i)
+    while population[0] == 5:
+        population.pop(0)
     population.append(0)
+
     year += 1        
 
 print(population)
