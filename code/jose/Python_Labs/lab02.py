0f1f3f3f6ded7eb1618aebc5391b87fcc93f6c49
Jose Cortes <jcortes8989@gmail.com>
2022-09-15 16:28:12 -0700

    making revisions to some of the previous labs

--- code/jose/Python_Labs/lab02.py
+++ code/jose/Python_Labs/lab02.py
@@ -58,16 +58,14 @@ tens_digitwritten = {2: 'twenty',
 if x in random_numbers:
     print (random_numbers[x])
 
-elif x in ones:
-    print(ones[x])
-elif 10 < x < 20:
-    print(tens_under20[x])
 
-elif x >= 20 and x < 100:
-    tens_digit = x//10
-    ones_digit = x%10
-    print(tens_digitwritten[tens_digit], ones[ones_digit])  
-
+if 110<=x < 119 or 220 <= x < 229 or 330 <= x < 339 or 440 <= x < 449 or 550 <= x < 559 or 660 <= x < 669 or 770 <= x < 779 or 880 <= x < 889 or 990 <= x < 999:
+    hundreds_digit = x//100
+    lst = []
+    lst.extend(x)
+    tens_digits =lst.pop(0)
+    print(tens_digits)
+    print(ones[hundreds_digit + tens_under20[tens_digits]])
 elif 100 <= x < 999:
     hundreds_digit = x//100
     x = str(x)
@@ -80,6 +78,39 @@ elif 100 <= x < 999:
     tens_digit = last_two_nums//10
     ones_digit = last_two_nums%10 
     print(hundreds_digit + 'hundred',tens_digitwritten[tens_digit],ones[ones_digit])
+
+
+elif x in ones:
+    print(ones[x])
+elif 10 < x < 20:
+    print(tens_under20[x])
+
+elif x >= 20 and x < 100:
+    tens_digit = x//10
+    ones_digit = x%10
+    print(tens_digitwritten[tens_digit], ones[ones_digit])  
+
+elif 100<=x < 109 or 200 <= x < 209 or 300 <= x < 309 or 400 <= x < 409 or 500 <= x < 509 or 600 <= x < 609 or 700 <= x < 709 or 800 <= x < 809 or 900 <= x < 909:
+    hundreds_digit = x//100
+    lst = []
+    x = str(x)                                  #added these lines to fix the in between spaces where the input would have been missed
+    lst.extend(x)
+    last_digit = lst.pop(0)
+    last_digit = lst.pop(1)
+    print(ones[hundreds_digit]+'hundred', ones[int(last_digit)])
+
+# elif 100 <= x < 999:
+#     hundreds_digit = x//100
+#     x = str(x)
+#     lst = []
+#     lst.extend(x)           #here I am simply taking the string, turning it into a string, then using the previous
+#     lst.pop(0)              #code to take the seperate numbers and concatenating them at the end
+#     last_two_nums = ''.join(lst)
+#     last_two_nums = int(last_two_nums)
+#     hundreds_digit = ones[hundreds_digit]
+#     tens_digit = last_two_nums//10
+#     ones_digit = last_two_nums%10 
+#     print(hundreds_digit + 'hundred',tens_digitwritten[tens_digit],ones[ones_digit])
 
 
     # firstwrd = num_ls(ones[0])
--- code/jose/Python_Labs/lab06.py
+++ code/jose/Python_Labs/lab06.py
@@ -8,6 +8,10 @@ letters_list = list(letters)
 user_string= input("Enter a word to encode it: ")
 user_list = list(user_string)
 
+encoding_letters = {1: 'a', 2: 'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', 10:'j', 11:'k', 12:'l', 13:'m', 14:'n', 15:'o',
+16:'p', 17:'q', 18:'r', 19:'s', 20:'t', 21:'u', 22:'v', 23:'w',24:'x',25:'y', 26:'z', 27:'a', 28:'b', 29:'c', 30:'d', 31:'e', 32:'f',
+33:'g', 34:'h', 35:'i', 36:'j', 37:'k', 38:'l', 39:'m'}
+
 index_numbers = []
 
 def find_index(a):
