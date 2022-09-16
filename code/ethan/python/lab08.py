5421b7379c91b2fbf032dcaeacba02ee6394bab0
VanguardAWS <ethanturner93@hotmail.com>
2022-09-16 18:22:15 -0500

    lab08 version 1 complete

---
+++ code/ethan/python/lab08.py
@@ -0,0 +1,37 @@
+# Contact List
+
+f = open("./contacts.csv", 'r', encoding='utf-8')
+
+contents = f.read()
+
+f.close()
+
+contents = contents.split('\n')
+
+length = len(contents)
+
+for i in range(length):
+    y = contents[i]
+    y = y.split(',')
+    contents[i] = y
+
+keys = contents[0]
+key_amount = len(keys)
+
+x = dict.fromkeys(keys, 1)
+
+del contents[0]
+
+y = 0
+
+for i in contents:
+    contacts = []
+    print(y)
+    for i in range(key_amount):
+        print(i)
+        x[keys[i]] = contents[y][i]
+        print(x)
+        contacts.append(x)
+    y += 1
+
+print(contacts)
