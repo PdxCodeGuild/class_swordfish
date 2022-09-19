87108dcd1e27e7ac7e8ca67696c095c9a8cbc31b
Yamil <yamil.nasser@outlook.com>
2022-09-19 18:35:30 -0500

    Updated Lab 8

--- code/yamil/python/lab_8.py
+++ code/yamil/python/lab_8.py
@@ -1,26 +1,63 @@
 import re
 
-players_list_outcome = [
-{"name" : "cristiano ronaldo", "current club" : "manchester united", "european league" : "premier league"},
-{"name" : "lionel messi", "current club" : "paris st germaine", "european league" : "ligue 1"},
-{"name" : "radamel falcao", "current club" : "rayo vallecano", "european league" : "la liga"}
-]
+# players_csv_outcome = [
+# {"name" : "cristiano ronaldo", "current club" : "manchester united", "european league" : "premier league"},
+# {"name" : "lionel messi", "current club" : "paris st germaine", "european league" : "ligue 1"},
+# {"name" : "radamel falcao", "current club" : "rayo vallecano", "european league" : "la liga"}
+# ]
 
 with open("csv_copy.csv", "r") as f:
     csv = f.read()
 
-csv_lines = csv.split("\n")
-tabs = csv_lines[0].split(",")
-# cr7 = full_list[1].split(",")
-# m10 = full_list[2].split(",")
-# km7 = full_list[3].split(",")
+lines = csv.split("\n")
+header = lines[0].split(",")
 
 player_list = []
-player_dict = {}
+player_csv = []
+new_line = "\n"
 
-for i in range(1,len(csv_lines)):
-    player = csv_lines[i].split(",")
+
+for i in range(1,len(lines)):
+    player = lines[i].split(",")
     player_list.append(player)
 
+for i in range(len(player_list)):
+    player_csv.append(dict(zip(header, player_list[i])))
+    
 
-# for i in range(full_list):
+def c():
+    added_info = []
+    name_add = input("Please enter the player's name: ")
+    added_info.append(name_add.lower())
+    team_add = input("Please enter the player's team: ")
+    added_info.append(team_add.lower())
+    league_add = input("Please enter the player's league: ")
+    added_info.append(league_add.lower())
+    return added_info
+
+with open("csv_copy.csv", "w") as f2:
+    f2.write(f"{csv}{new_line}{','.join(c())}")
+
+
+def r():
+    retrieve = input("Which player would you like to see?: ")
+    for i in range(len(player_csv)):
+        if player_csv[i]['name'].lower() == retrieve.lower():
+            print(player_csv[i])
+            return(player_csv[i])
+    if player_csv[i]['name'] != retrieve:
+        print("Player is not currently active.")
+
+def u():
+    update = input("Which player would you like to update?:")
+    for i in range(len(player_csv)):
+        if player_csv[i]['name'].lower() == update.lower():
+            update_question = input("What attribute would you like to update?: Club or League? ")
+            if update_question.lower() == "club":
+                update_attribute = input("Please enter the new club: ")
+            elif update_question.lower() == "league":
+                update_attribute = input("Please enter the new league: ")
+            else:
+                print("Please enter either club or league.")
+        print(update_attribute)
+        return update_attribute
