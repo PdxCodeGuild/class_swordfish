6c331329184f65501ac57f557e3cdcd5136ab533
VanguardAWS <ethanturner93@hotmail.com>
2022-09-19 18:47:25 -0500

    ethan lab08 in progress

--- code/ethan/python/lab08.py
+++ code/ethan/python/lab08.py
@@ -4,7 +4,7 @@ f = open("./contacts.csv", 'r', encoding='utf-8')
 
 contents = f.read()
 
-f.close()
+# f.close()
 
 contents = contents.split('\n')
 
@@ -32,3 +32,25 @@ for i in contents:
     y += 1
 
 print(contacts)
+
+choice = input('''Welcome to your contact list.
+Type "1" to create a record.
+Type "2" to retrieve a record.
+Type "3" to update a record.
+Type "4" to delete a record.
+ ''')
+
+updated_contents = []
+
+if choice == '1':
+    
+    name = input('Enter the name of the contact you would like to add: ')
+    favorite_fruit = input('Enter their favortire fruit: ')
+    favorite_color = input('Enter their favorite color: ')
+    updated_contact = name + ' ' + favorite_fruit + ' ' + favorite_color
+    updated_contact = list(updated_contact)
+    print(updated_contact)
+
+    contents = contents.append(name)
+
+print(contents)
