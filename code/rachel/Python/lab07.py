eb5e1de0faeb6c72baae5e538a45781358da240b
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2022-09-20 18:16:22 -0400

    Lab 7 Completed

--- code/rachel/Python/lab07.py
+++ code/rachel/Python/lab07.py
@@ -1,12 +1,10 @@
 #Compute Automated Readability Index
-# Save a text file containing the Gettysburg Address (copied from internet)
-    #copy just text into a .txt file
+# Save a text file containing the Gettysburg Address (copied from internet); copy just text into a .txt file
 # Open the file in python
 # Seperate the file into a few different lists:
     # Characters
     # Words
     # Sentences (find & count periods)
-# Count the number of each (return len)
 # Compute the ARI using the formula in the lab instructions
 # Copy dicitonary into code and compare ARI output against it
 # Print output message
@@ -14,67 +12,77 @@
 filename = 'gettysburg.txt'
 
 with open(filename) as gettysburg_file:
-    lines = gettysburg_file.readlines() 
+    lines = gettysburg_file.readlines()
 
-#return word count:
-# for line in lines:
-#     text_string = line.rstrip()
-#     words = line.split()
-#     word_count = len(words)
-#     print(word_count) returns 30 72 162, each on their own line
-
-# #return letter count
-# for line in lines:
-#     text_string = line.rstrip()
-#     char_count = len((ele for ele in text_string if ele.isalpha())
-#     print(char_count)
-
-#turning above into a function in order to turn output (word_count) into a list:
-def number_of_words():
-    total_words = 0
-    #total_words_list = []
+def lines_of_text():
     for line in lines:
         text_string = line.rstrip()
+    return text_string
+
+def words():
+    total_words = 0
+    for line in lines:
+        #text_string = line.rstrip()
         words = line.split()
         word_count = len(words) 
         total_words += word_count
-        #total_words_list.append(word_count)
     return total_words
 
-# or to return a list
-def number_of_words():
-    #total_words = 0
-    total_words_list = []
+def letter():
+    letter_count = 0
+    text_string = lines_of_text()
+    for char in text_string:
+        if char.isalpha():
+            letter_count += 1
+    return letter_count
+
+def sentence():
+    total_sentences = 0
     for line in lines:
-        text_string = line.rstrip()
-        words = line.split()
-        word_count = len(words) 
-        #total_words += word_count
-        total_words_list.append(word_count)
-    return total_words_list
+        #text_string = line.rstrip()
+        sentences = line.split(". ")
+        sentence_count = len(sentences)
+        total_sentences += sentence_count
+    return total_sentences
 
-total_words = []
-for i in total_words:
-    word_count = number_of_words()
-    #total_words.extend(word_count)
-    total_words.append(word_count)
-print(total_words)
+# print(words())
+# print(letter())
+# print(sentence())
 
+def ARI():
+    total_words = words()
+    letter_count = letter()
+    total_sentences = sentence()
+    char_words = (letter_count / total_words) * 4.71
+    word_sent = (total_words / total_sentences) * 0.5
+    final_number = (char_words + word_sent) -21.43
+    round_number = round(final_number)
+    return round_number
 
-#turn word_count output into a list IOT sum the elements
-# word_count = [] 
-# for i in word_count:
-#     word_count.append(i)
-# print(word_count)
+# print(ARI())
 
-# total_words = 0
-# for i in word_count:
-#     total_words += i
-# print(total_words)
+ari_scale = {
+     1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+     2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+     3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+     4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+     5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+     6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+     7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+     8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+     9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+    10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+    11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+    12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+    13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+    14: {'ages': '18-22', 'grade_level':      'College'}
+}
 
+ARI_grade = ari_scale[ARI()].get('grade_level')
+ARI_age = ari_scale[ARI()].get('ages')
+# print(ARI_level)
 
-
-
-
-
+print(f'The ARI for the gettysburg address is {ARI()}.')
+#print(f'This corresponds to a {ari_scale[ARI()].get('grade_level')}level of diffitculty') >> can this be turned into one line so that the variables "ÄRI_grade" and "ARI_age" are needed?
+print(f'This corresponds to a {ARI_grade} level of diffitculty that is suitable for an average person {ARI_age} years old.')
 
