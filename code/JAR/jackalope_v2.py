4cb9d0c2a893b82238fe4ec32b813b2ec111328a
Jack Amoss <jackamoss@Jacks-MacBook-Pro.local>
2022-09-20 16:01:45 -0700

    add population plotting

--- code/JAR/jackalope_v2.py
+++ code/JAR/jackalope_v2.py
@@ -1,57 +1,58 @@
-#Mob Programming: Jackalope Simulator
+#Mob Programming: newbornsalope Simulator
 
 import random
-import re
+import matplotlib.pyplot as plt
+import numpy as np
 
-#birth function to return newborn jackalope info
+#birth function to return newborn newbornsalope info
 def birth():
-    jack_dict = {
+    newborn = {
         # 'name': not sure how to approach this
         'age': 0, 
         'sex': random.choice(['Male', 'Female']),
         'pregnant': False
     }
-    return jack_dict
+    return newborn
 
-population = [{'age': 0, 'sex': 'Male', 'pregnant': False}, {'age': 0, 'sex': 'Female', 'pregnant': False}] # jackalopes are stored as dict in list
+population = [{'age': 0, 'sex': 'Male', 'pregnant': False}, {'age': 0, 'sex': 'Female', 'pregnant': False}] # newbornsalopes are stored as dict in list
 # population = [birth() for i in range(3)]
 year = 0
 
 #returns list of all newborn dictionaries
 def pop_increase():
-    jack = []
+    newborns = []
     for i in population: 
         if i['pregnant'] == True:
-            jack.append(birth())
+            newborns.append(birth())
         i['pregnant'] = False
-    return jack
+    return newborns
 
 # Age check for females
 def age_check(i):
-    if 4 <= population[i]['age'] <= 8:
+    if 4 <= population[(i + 1) % len(population)]['age'] <= 8:
         return True
     else:
         return False
 
 # Age check for males
 def male_check(i):
-    if 4 <= population[(i + 1) % len(population)]['age'] <= 8 or 4 <= population[(i - 1) % len(population)]['age'] <= 8:
+    if age_check(i + 1) or age_check(i - 1):
         return True
     else:
         return False
 
 #sets pregnancy status for females
-def sex():
+def mate():
     for i in range(len(population)):
         if age_check(i) == False:
             continue
         if population[i]['sex'] == 'Female':
-            #checks if next to male jackalope (connects list ends)
+            #checks if next to male newbornsalope (connects list ends)
             if population[(i + 1) % len(population)]['sex'] == 'Male' or population[(i - 1) % len(population)]['sex'] == 'Male':
                 if male_check(i) == True:
                     population[i]['pregnant'] = True
 
-# Death function removes all jackalopes at the age of 10.
+# Death function removes all newbornsalopes at the age of 10.
 def death():
     pop_new = list(reversed(sorted(population, key=lambda d: d['age']))) # Sorts through the list of population and puts the oldest at the front.
     for i in range(len(pop_new)):  # Runs through the new list and removes anything with above the death age.
@@ -59,21 +60,49 @@ def death():
             pop_new.pop(0)
     return pop_new
 
+pop_r_total = []
+pop_r_males = []
+pop_r_females = []
+def census(population):
+    males = 0
+    females = 0
+    for i in population:
+        if i['sex'] == 'Male':
+            males += 1
+        else:
+            females += 1
+    return ((males, females))
+
+
 #each loop iteration is 1 year
 while len(population) < 1000:
     for i in range(len(population)):
-        population[i]['age'] += 1 #jackalope ages 1 year
-    sex()
+        population[i]['age'] += 1 #newbornsalope ages 1 year
+    
     year +=1
+    mate()
     population.extend(pop_increase())
     population = death()
+
+    census_year = census(population)
+    pop_r_total.append(sum(census_year))
+    pop_r_males.append(census_year[0])
+    pop_r_females.append(census_year[1])
+
     random.shuffle(population)
     if len(population) == 0:
         print('Everyone is dead!')
         break
 
-# print(death())
-# print(len(population))
-# print([[population[i]['sex'], population[i]['pregnant'], population[i]['age']]for i in range(len(population))])
-print(year)
-# print(death_num)
+
+#setting variables to plot
+plt.plot(pop_r_total, 'm', label = 'total')
+plt.plot(pop_r_males, 'r', label = 'males')
+plt.plot(pop_r_females, 'b', label = 'females')
+plt.legend(loc = "upper left")
+plt.xlabel('Year')
+plt.ylabel('Population')
+
+plt.show()
+
+print(f'years: {year}')
