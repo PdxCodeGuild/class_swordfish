429e8cf54254d12a2ebc8e8e5826f0823fe71599
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2022-09-23 19:28:22 -0400

    Lab 9 Completed

--- code/rachel/Python/lab09.py
+++ code/rachel/Python/lab09.py
@@ -1,7 +1,9 @@
 class ATM:
+
     def __init__(self):
         self.balance = 0
         self.interest_rate = 0.1
+        self.transactions = []
     
     def check_balance(self):
         """Returns the account balance"""
@@ -9,23 +11,29 @@ class ATM:
 
     def deposit(self, amount):
         """Deposits the given amount in the account"""
-        self.new_balance = amount + self.balance
-        #return f"{amount}"
+        self.balance = amount + self.balance
+        self.transactions.append(f'user deposited {amount}')
         return amount
-        
+  
     def check_withdrawal(self, amount):
         """Returns true if the withdrawn amount won't put the account in the negative"""
-        amount -= self.new_balance
+        if self.balance >= amount:
+            return True
 
     def withdraw(self, amount):
         """Withdraw the amount from the account and returns it"""
-        #self.remaining_balance = self.new_balance - amount
+        self.transactions.append(f'user withdrew {amount}')
         return amount
 
     def calc_interest(self):
         """Returns the amount of the interest calculated on the account"""
-        self.earned_interest = self.new_balance * self.interest_rate
+        self.earned_interest = (self.balance * self.interest_rate) / 100
         return self.earned_interest
+
+    def print_transactions(self):
+        for transaction in self.transactions:
+            print(transaction)
+
 
 atm = ATM() # create an instance of our class
 print('Welcome to the ATM')
@@ -37,25 +45,28 @@ while True:
     elif command == 'deposit':
         amount = float(input('How much would you like to deposit? '))
         atm.deposit(amount) # call the deposit(amount) method
-        print(f'Deposited ${amount}')
+        print(f'Deposited ${amount:.2f}')
     elif command == 'withdraw':
         amount = float(input('How much would you like '))
         if atm.check_withdrawal(amount): # call the check_withdrawal(amount) method
             atm.withdraw(amount) # call the withdraw(amount) method
-            print(f'Withdrew ${amount}')
+            print(f'Withdrew ${amount:.2f}')
         else:
             print('Insufficient funds')
     elif command == 'interest':
         amount = atm.calc_interest() # call the calc_interest() method
         atm.deposit(amount)
-        print(f'Accumulated ${amount} in interest')
+        print(f'Accumulated ${amount:.2f} in interest')
     elif command == 'help':
         print('Available commands:')
         print('balance  - get the current balance')
         print('deposit  - deposit money')
         print('withdraw - withdraw money')
         print('interest - accumulate interest')
+        print('transactions - view transaction log')
         print('exit     - exit the program')
+    elif command == 'transactions':
+        atm.print_transactions()
     elif command == 'exit':
         break
     else:
