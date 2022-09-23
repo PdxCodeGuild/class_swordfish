b3ac00d411ea15a95b0e12e3a20f2e3a38856bef
Alden Addor <alden@Aldens-MacBook-Air.local>
2022-09-23 16:07:30 -0600

    Lab 10 v2 complete. tictactoe work in progress.

--- code/alden/python/lab10.py
+++ code/alden/python/lab10.py
@@ -1,45 +1,39 @@
-# Tik-tak-toe
+# Dad joke API
+'''
+# from pydoc import pager
+# from xml.etree.ElementInclude import LimitedRecursiveIncludeError
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Can someone tell me why things like this auto import into my code while I am working on it please!!
+'''
+import requests
 
-class Player():
-    def __init__(self, name, token):
-        self.name = name
-        self.token = token
-
-class Board():
-    def __init__(self):
-        self.board = [[' ' for r in range(3)] for r in range(3)]  # Creates a list of 3 lists with ' ' holder for the token later.
-        # print(self.board)
-
-    def __repr__(self):
-        rows = ''
-        for i, sub in enumerate(self.board): # Enumerates through list of list to space things into a grid.
-            rows += '|'.join(sub)
-            rows += '\n'
-            if i < 2:
-                rows += '- - -'
-                rows += '\n'
-        return rows
-
-    def move(self, x, y, player):
-        if self.board[x][y] != ' ':
-            print('Space is taken.')
-        else:
-            self.board[x][y] = player
-            return self.board
-
-    def calc_winner(self, player):
-        for i in range(len(self.board)):     # Use zip in for loop to get vertical solution.
-            if all(self.board[i]) == True and self.board[i] != ' ': # horizonal check
-                print(f'{player} is the winner!!')
-        else:
-            print('check')
-            # elif self.board[i]
-            # print(i) #sub)
-            
-
-board = Board()
-board.move(0, 0, 'X')
-board.move(0, 1, 'X')
-board.move(0, 2, 'X')
-print(board)
-print(board.calc_winner('X'))
+params = {}     # Place holder for the parameters in the request.get
+while True:
+    search = input('Please enter search peramiters(p, l, t) or help: ') # User prompt
+    data = requests.get('https://icanhazdadjoke.com/search', headers = {'accept':'application/json'},  params = params)
+    result = data.json()
+    
+    if search == "help":        # Shows all the possible inputs to the user.  Prompted to it if user doesn't input a correct command.
+        print('Available searches:')
+        print('p = page  (default:1)')
+        print('l = limit (default:20 max:30)')
+        print('t = term  (defaults: list all jokes)')
+        print('pr = print jokes')
+        print('e = exit')
+    elif search == 'p':         # Creates a page number to search.
+        p = input('What page number would you like: ')
+        params.update({'page':p})
+    elif search == 'l':         # Creates how many jokes you want to see.
+        l = input('How many jokes would you like: ')
+        params.update({'limit':l})
+    elif search == 't':         # Allows to search for terms in the joke.
+        t = input('Is there a term you would like: ')
+        params.update({'term':t})
+    elif search == 'pr':        # Prints out jokes based on the current search parameters.
+        for joke in result['results']:
+            print(f'{joke["joke"]}\n')
+    elif search == 'e':         # Exits loop
+        break
+    else:
+        print('Command not recognized. Enter "help" for assistance.')
+        
---
+++ code/alden/python/tictactoe.py
@@ -0,0 +1,46 @@
+# Tik-tak-toe
+
+class Player():
+    def __init__(self, name, token):
+        self.name = name
+        self.token = token
+
+class Board():
+    def __init__(self):
+        self.board = [[' ' for r in range(3)] for r in range(3)]  # Creates a list of 3 lists with ' ' holder for the token later.
+        # print(self.board)
+
+    def __repr__(self):
+        rows = ''
+        for i, sub in enumerate(self.board): # Enumerates through list of list to space things into a grid.
+            rows += '|'.join(sub)
+            rows += '\n'
+            if i < 2:
+                rows += '- - -'
+                rows += '\n'
+        return rows
+
+    def move(self, x, y, player):
+        if self.board[x][y] != ' ':
+            print('Space is taken.')
+        else:
+            self.board[x][y] = player
+            return self.board
+
+    def calc_winner(self, player):
+        for i in range(len(self.board)):     # Use zip in for loop to get vertical solution.
+            if all(self.board[i]) == True: # horizonal check
+                if self.board[i] != ' ':
+                    print(f'{player} is the winner!!')
+        else:
+            print('check')
+            # elif self.board[i]
+            # print(i) #sub)
+            
+
+board = Board()
+board.move(0, 0, 'X')
+board.move(0, 1, 'X')
+board.move(0, 2, 'X')
+print(board)
+print(board.calc_winner('X'))
