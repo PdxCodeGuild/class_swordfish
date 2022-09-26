c16b305a7ba7cf7ca556ac529b8dac9d47c4f68f
Julia Chobu <juliachobu@Julias-Air.lan>
2022-09-25 22:27:14 -0700

    labs

--- code/julia/Python/lab05.py
+++ code/julia/Python/lab05.py
@@ -1,1 +1,27 @@
 #Credit Card Validation
+
+def credit_card_validator(orig_num):
+    list_digits = list(orig_num)
+    for i in range(len(list_digits)):
+        list_digits[i] = int(list_digits[i])
+    check_digit = list_digits.pop()
+
+
+    list_digits.reverse()
+    for i in range(len(list_digits)):
+        if i % 2 == 0:
+          
+            return list_digits
+
+
+    
+    # every_second_num = list(list_digits)
+    # for i in range(1, len(), *2):
+    #     every_second_num.append(list_digits[i])
+        
+
+
+
+print(credit_card_validator("4556737586899855"))
+
+
---
+++ code/julia/lab07.py
@@ -0,0 +1,34 @@
+#ARI
+import math
+#number of characters
+file = open("myfile.txt", "r")
+myfile = file.read().replace(" ","")
+characters = len(myfile)
+print('Number of characters in text file: ', characters)
+
+#number of words
+file = open("myfile.txt", "r")
+myfile = file.read()
+words = myfile.split()
+print('Number of words in text file: ', len(words))
+
+#number of sentences
+with open("myfile.txt") as f:
+    data = f.read()
+    print ("total stops: ", data.count("."))
+
+
+
+
+words = 280
+characters = 1359
+sentences = 11
+#math.ceil(4.71)
+x = 4.71
+y = 21.43
+z = .5
+
+score = float(x / characters / words / + z / words / sentences / - y)
+
+print("Score: ", score)
+
