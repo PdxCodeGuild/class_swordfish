823aea14e8f80b7989c58aab8e957122fafede9b
Alden Addor <alden@Aldens-MacBook-Air.local>
2022-09-26 17:34:16 -0600

    tictactoe continued

--- code/alden/python/tictactoe.py
+++ code/alden/python/tictactoe.py
@@ -5,6 +5,15 @@ class Player():
         self.name = name
         self.token = token
 
+    def __repr__(self):
+        return self.token
+
+    def player_2(self):
+        if player_1 == 'O':
+            player_2 == 'x'
+        else:
+            player_2 == 'O'
+
 class Board():
     def __init__(self):
         self.board = [[' ' for r in range(3)] for r in range(3)]  # Creates a list of 3 lists with ' ' holder for the token later.
@@ -27,22 +36,63 @@ class Board():
             self.board[x][y] = player
             return self.board
 
-    def calc_winner(self, player): # Use zip in for loop to get vertical solution.
+    def calc_winner(self): # Use zip in for loop to get vertical solution.
+        vert = list(zip(*self.board))
+        for i in range(3):
+            vert_check = vert[i]
+            check = [s==vert_check[0] and s != ' ' for s in vert_check]
+            if all(check):
+                return f'{vert_check[0]} wins!'
+        
         for i in range(3):
             hori = self.board[i]        # horizonal check
-            list = [s==hori[0] and s != ' ' for s in hori] # creates a list of booleans
-            if all(list):           # Checks that list of booleans are all True.
+            row = [s==hori[0] and s != ' ' for s in hori] # creates a list of booleans
+            if all(row):           # Checks that list of booleans are all True.
                     return f'{hori[0]} wins!'
-            
+
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board != ' ':
+            return f'{self.board[0][0]} wins!'
+
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board != ' ':
+            return f'{self.board[0][2]} wins!'
         else:
-            print('check')
-            # elif self.board[i]
-            # print(i) #sub)
+            pass
+
+    def is_full(self):
+        for i in self.board:
+            if any(posi == ' ' for posi in i):
+                return False
+        else:
+            True
             
+    def is_game_over(self):
+        if self.is_full() == True:
+            return f'Tied game'
+        else:
+            return self.calc_winner()
 
 board = Board()
-board.move(1, 0, 'X')
-board.move(1, 1, 'X')
-board.move(1, 2, 'X')
-print(board)
-print(board.calc_winner('X'))
+player_1 = Player('Alden', 'X')
+player_2 = Player('Joe', 'O')
+round = 1
+print(player_1)
+print(player_2)
+while True:
+    p1_move = Board.move(input('Input x coordinate: '), input('Input y coordinate: '), player_1) # Keep getting error "TypeError: Board.move() missing 1 required positional argument: 'player'"
+    p2_move = Board.move(input('Input x coordinate: '), input('Input y coordinate: '), player_2)
+    round += 1
+
+    if round >= 3:
+        Board.is_game_over()
+
+    if Board.is_game_over() == True:
+        print(board)
+        break
+
+
+
+# board.move(0, 0, 'X')
+# board.move(1, 0, 'X')
+# board.move(2, 0, 'X')
+# print(board)
+# print(board.is_full())
