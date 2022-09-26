9a86940b27580ef91fcc7575428e6e2b481f4d69
Leslie <leslie.l.naylor@gmail.com>
2022-09-26 13:42:53 -0400

    lab10 update

--- code/leslie/Python/lab10.py
+++ code/leslie/Python/lab10.py
@@ -1,12 +1,12 @@
 import requests
 import pprint
 
-#response = requests.get('https://icanhazdadjoke.com/', headers = {'Accept': 'application/json'}, params={'User-Agent': '(https://github.com/PdxCodeGuild/class_swordfish/code/leslie)'})
+response = requests.get('https://icanhazdadjoke.com/', headers = {'Accept': 'application/json'}, params={'User-Agent': '(https://github.com/PdxCodeGuild/class_swordfish/code/leslie)'})
 
-#the_joke = response.json()["joke"]
-#print(the_joke)
+the_joke = response.json()#["joke"]
+print(the_joke)
 
-
+#VERSION 2
 while True:
     jokes_keyword = input("What would you like to see jokes about?: ")
     response = requests.get('https://icanhazdadjoke.com/search', headers = {'Accept': 'application/json'}, params={'User-Agent': '(https://github.com/PdxCodeGuild/class_swordfish/code/leslie)', 'limit': 3, 'term': jokes_keyword})
