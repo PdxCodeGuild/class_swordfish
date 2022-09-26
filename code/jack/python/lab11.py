8ae1cade54c3ce303106bb95eed4cd8654bd613e
Jack Amoss <jackamoss@Jacks-MacBook-Pro.local>
2022-09-26 11:31:08 -0700

    lab 11 v2 complete

--- code/jack/python/lab11.py
+++ code/jack/python/lab11.py
@@ -2,11 +2,42 @@
 
 import requests
 
-def get_quote():
-    response = requests.get('https://favqs.com/api/qotd', headers={'accept': 'application/json'})
-    results = response.json()
-    quote = results['quote']['body']
-    author = results['quote']['author']
-    print(f'{quote} -{author}')
+parameters = {'page': 1}
 
-get_quote()
+def get_quotes(): # returns list of quote dictionaries
+    response = requests.get('https://favqs.com/api/quotes', headers={'accept': 'application/json','Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}, params = parameters)
+    results = response.json()
+    return results['quotes']
+  
+
+def quotes_to_print(quotes): # takes list of quote dictionaries and prints quote + author for each dict
+    print(f'{len(quotes)} quotes associated with {keyword} - page {parameters["page"]}')
+    for dict in quotes:
+        quote = dict['body']
+        author = dict['author']
+        output = '-' + quote + ' -' + author
+        print(output)
+        
+
+
+
+keyword = input('Enter a keyword to search for quotes: ')
+parameters['filter'] = keyword
+print('\n')
+quotes = get_quotes()
+quotes_to_print(quotes)
+
+
+
+# REPL
+while True:
+    command = input('Enter \'next page\' or \'done\': ')
+
+    if command == 'done':
+        break
+    elif command == 'next page':
+        parameters['page'] += 1
+        quotes = get_quotes()
+        quotes_to_print(quotes)
+    else:
+        print('Invalid input')
