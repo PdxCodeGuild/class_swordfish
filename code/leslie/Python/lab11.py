c10bafc22154d02631ef58526e21fd3ccf5b890d
Leslie <leslie.l.naylor@gmail.com>
2022-09-27 19:34:12 -0400

    lab 11 almost complete

--- code/leslie/Python/lab11.py
+++ code/leslie/Python/lab11.py
@@ -4,33 +4,52 @@ import json
 
 response = requests.get('https://favqs.com/api/qotd', headers = {'Content-Type': 'application/json', 'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
 
-random_quote = json.loads(response.text)['quote']['body']
-author = json.loads(response.text)['quote']['author']
+#random_quote = json.loads(response.text)['quote']['body']
+#author = json.loads(response.text)['quote']['author']
+#data = response.json()
+#pprint.pprint(data)
 #print(random_quote, "--", author)
 
-#Version 2 -- 25 results per page -- how do I go to the next 25 pages??
+
+
+#Version 2 
 while True:
-    keyword = input("Please enter keyword to search for quotes: ")
     
-    res = requests.get('https://favqs.com/api/quotes/?filter=<keyword>', headers = {'Content-Type': 'application/json', 'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}, params={'filter': keyword, 'page': 1})
-    quotes = json.loads(res.text)['quotes']
+    keyword = input("Please enter keyword to search for quotes: ")
+    my_params={'filter': keyword}
+    res = requests.get('https://favqs.com/api/quotes', headers = {'Content-Type': 'application/json', 'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}, params = my_params)
+    quotes = res.json()['quotes']
+    
+    
     for dic in quotes:
         for key in dic:
             if key == "body":
                 print(dic[key],'\n')
-    next = input("Type 'next': next 25 quotes. 'new': new keyword search, or type 'exit' to exit: ")
-    if next == "exit":
-        break
-    if next == "new":
-        
-
     
-#first_quote = quotes[0]
-#print(first_quote)
-#the_author = first_quote['author']
-#the_same_author = json.loads(res.text)['quotes'][0]['author']
-#print(the_same_author)
-#print(the_author)
-#print(type(quotes))
+               
+    last_page = res.json()['last_page']
+    print("Last page? ", last_page)
+    page = res.json()['page']
+    print("Page ", page)
 
-#pprint.pprint(quotes)
+    while last_page == False:
+        next = input("To see the next page, type 'next'. To search by new keyword, please type new search term. To quit, type 'quit.'  ")
+        if next == "next":
+            page = res.json()['page'] + 1            
+            my_params={'filter': keyword, 'page': page}
+            res = requests.get('https://favqs.com/api/quotes', headers = {'Content-Type': 'application/json', 'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}, params = my_params)
+            quotes = res.json()['quotes']
+            for dic in quotes:
+                for key in dic:
+                    if key == "body":
+                        print(dic[key],'\n')
+            last_page = res.json()['last_page']
+            print("Last page? ", last_page)
+            page = res.json()['page']
+            print("Page ", page)
+                          
+        elif next == 'quit':
+            break
+    
+        
+         
