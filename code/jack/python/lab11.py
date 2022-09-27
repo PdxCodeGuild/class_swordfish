e3d5fab07dd3882b3b7e758484a94ef65278461c
Jack Amoss <jackamoss@Jacks-MacBook-Pro.local>
2022-09-27 09:44:32 -0700

    added new term search option

--- code/jack/python/lab11.py
+++ code/jack/python/lab11.py
@@ -9,29 +9,35 @@ def get_quotes(): # returns list of quote dictionaries
     results = response.json()
     quotes = results['quotes']
 
-    print(f'{len(quotes)} quotes associated with {keyword} - page {parameters["page"]}')
-    for dict in quotes: # prints each quote/author from list of quote dictionaries
-        quote = dict['body']
-        author = dict['author']
-        output = '-' + quote + ' -' + author
-        print(output)
+    if quotes[0]['body'] == 'No quotes found':
+        print(quotes[0]['body'])
+    else:
+        print(f'{len(quotes)} quotes associated with {parameters["filter"]} - page {parameters["page"]}')
+        for dict in quotes: # prints each quote/author from list of quote dictionaries
+            quote = dict['body']
+            author = dict['author']
+            output = '-' + quote + ' -' + author
+            print(output)
     
 
 # collect keyword, first output
-keyword = input('Enter a keyword to search for quotes: ')
-parameters['filter'] = keyword
+parameters['filter'] = input('Enter a keyword to search for quotes: ')
 print('\n')
 get_quotes()
 
 
 # REPL for subsequent pages of quotes
 while True:
-    command = input('Enter \'next page\' or \'done\': ')
+    command = input('Enter \'next page\', \'done\', or \'new term\': ')
 
     if command == 'done':
         break
     elif command == 'next page':
         parameters['page'] += 1
+        get_quotes()
+    elif command == 'new term':
+        parameters['page'] = 1
+        parameters['filter'] = input('Enter a new keyword to search for quotes: ')
         get_quotes()
     else:
         print('Invalid input')
