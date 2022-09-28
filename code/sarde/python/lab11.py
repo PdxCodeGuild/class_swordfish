9634884b1c89ee9032dd27ea647b1b5cee20da47
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-09-28 19:15:45 -0400

    Lab11 Finished

--- code/sarde/python/lab11.py
+++ code/sarde/python/lab11.py
@@ -1,79 +1,116 @@
+from ast import While
+import requests
+import pprint
 '''Lab 11: Quotes API
 '''
 '''
--using the Favqs Quotes API to first get a random quote,
--and then allow the user to find quotes by keyword
+-Using the Favqs Quotes API to first get a random quote
+-And then allow the user to find quotes by keyword
+-To accomplish this we'll be using the requests library.
 '''
-# get random quote
-import requests
-import pprint
-response = requests.get(
-    'https://favqs.com/api/qotd', headers={'accept': 'application/json'})  # -->get random quote
-# print(response.text)
-quote = response.json()['quote']['body']
-author = response.json()['quote']['author']
-# print(f'Quote: {quote} Author: {author}')
+'''Version 1: Get a Random Quote
+    -The URL to get a random quote is https://favqs.com/api/qotd,
+    -Send a request here,
+    -Parse the JSON in the response into a python dictionary,
+    -and show the quote and the author.
+'''
+'''
+response = requests.get('https://favqs.com/api/qotd')
+# print(response)  # --> <Response [200]>
+# print(response.text)  # -->class str --> raw response
+# parse JSON object into a dictionary
+json_dict = response.json()
+# print(json_dict)  # --> class dict
+# print(json_dict.keys())
+# dict_keys(['qotd_date', 'quote'])
+quote = json_dict['quote']['body']
+# print(quote)
+author = json_dict['quote']['author']
+# print(f'Author: {author}  Quote: {quote}')
+'''
+'''Version 2: List Quotes by Keyword
+    -Prompt the user for a keyword,
+    -List the quotes you get in response
+    -Prompt the user to either show the next page
+    -Or enter a new keyword
+'''
+
+
+# def get_quotes():
+#     response = requests.get(f'https://favqs.com/api/quotes?page={page}&filter={keyword}',
+#                             headers={'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+#     # print(response.text)
+#     quotes_dict = response.json()
+#     # print(type(quotes_dict))  # --> class dict
+#     quotes_list = quotes_dict['quotes']
+#     # print(type(quotes_list))  # --> class list
+
+#     for quote in quotes_list:
+#         author = quote['author']
+#         quote = quote['body']
+#         print(f'Author: {author}\nQuote: {quote}\n')
+
 
 keyword = input('Enter a keyword to search for quotes: ')
-response = requests.get(f'https://favqs.com/api/quotes?filter={keyword}',
+response = requests.get(f'https://favqs.com/api/quotes?page=<page>&filter={keyword}',
                         headers={'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
-json_response = response.json()
-quotes_list = json_response['quotes']
+# print(response.text)  # --> class str
+quotes_dict = response.json()
+# print(quotes_dict)  # --> class dict
+# print(quotes_dict.keys())
+# dict_keys(['page', 'last_page', 'quotes'])
+quotes_list = quotes_dict['quotes']
+# print(quotes_list)  # --> class list
+
+# iterate over the quote_list
 for quote in quotes_list:
     author = quote['author']
     quote = quote['body']
-    print('\n')
-    print(author)
-    print(quote)
+    print(f"Author: {author}\nQuote: {quote}")
 
-page = json_response['page']
-print('page', page)
-print('last_page', json_response['last_page'])
+page = quotes_dict['page']
+last_page = quotes_dict['last_page']
+print(f'page: {page}')  # --> page: 1
+print(f'last page: {last_page}')  # --> last page: True
 
 
+# REPL loop
 while True:
     user_input = input('Enter "next page", "new search", "done": ')
     if user_input == 'next page':
-        print('User wants next page')
-        if json_response['last_page'] == False:
+        print('User wants to go to the next page')
+        # page
+        # keyword --> user wants the same search, and to go to the next page
+        if quotes_dict['last_page'] == False:
             page += 1
         else:
-            print('This is the end of your search')
-            continue
+            print('No more pages')
+        # send a request to the API for the next page, after the loop
 
     elif user_input == 'new search':
         print('User wants a new search')
+        keyword = input('Enter a new search word: ')
+        print(keyword)
+
     elif user_input == 'done':
         print('User wants to exit the program')
         break
+
     else:
-        # -->The continue keyword is used to end the current iteration in a for loop (or a while loop), and continues to the next iteration.
+        print('Invalid user input, Try again')
         continue
 
-        # allow the user to find quotes by keyword
+    print('next page', page)
+
     response = requests.get(f'https://favqs.com/api/quotes?page={page}&filter={keyword}',
                             headers={'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
-
-    # print(type(response))  # --> class Response object
-    json_response = response.json()
-    # print(type(json_response))  # --> class dict
-    # print(json_response.keys())  # -->dict_keys(['page', 'last_page', 'quotes'])
-    quotes_list = json_response['quotes']
+    # print(response.text)
+    quotes_dict = response.json()
+    # print(type(quotes_dict))  # --> class dict
+    quotes_list = quotes_dict['quotes']
     # print(type(quotes_list))  # --> class list
-    the_first_quote = quotes_list[0]
-    # print(type(the_first_quote))  # --> class dict
-    # print(the_first_quote.keys())
-    # dict_keys(['id', 'dialogue', 'private', 'tags', 'url', 'favorites_count', 'upvotes_count', 'downvotes_count', 'author', 'author_permalink', 'body'])
-    quote_body = the_first_quote['body']
-    # print(quote_body)
 
-   # print(type(quotes_list[1]))
     for quote in quotes_list:
         author = quote['author']
         quote = quote['body']
-        print('\n')
-        print(author)
-        print(quote)
-
-    print('page', json_response['page'])
-    print('last_page', json_response['last_page'])
+        print(f'Author: {author}\nQuote: {quote}\n')
