433c1a60d0fa5ea05dc1868f42dc657a388a49fb
Yamil <yamil.nasser@outlook.com>
2022-09-29 13:04:04 -0500

    Revised Lab 11

--- code/yamil/python/lab_11.py
+++ code/yamil/python/lab_11.py
@@ -46,9 +46,18 @@ for i in quote_dict['quotes']:
     print(i['body'],"\n")
 
 while True:
-    next_or_end = input("Would you like to see 'next page' or 'end'?: ")
+    next_or_end = input("Would you like to see 'next page', 'new search', or 'end'?: ")
     if next_or_end == 'next page':
         page += 1
+        response = requests.get(f"https://favqs.com/api/quotes?page={page}&filter={quote_keyword}", headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
+        quote_dict = response.json()
+        quote_counter = len(quote_dict['quotes'])
+        print(f"{quote_counter} quotes associated with '{quote_keyword}' in page {page}.","\n")
+        for i in quote_dict['quotes']:
+            print(i['body'],"\n")
+    elif next_or_end == 'new search':
+        quote_keyword = input("Enter a keyword to search for quotes: ")
+        page = 1
         response = requests.get(f"https://favqs.com/api/quotes?page={page}&filter={quote_keyword}", headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
         quote_dict = response.json()
         quote_counter = len(quote_dict['quotes'])
