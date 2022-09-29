6554aeae4995c02f9cdba169301877c51b38eed0
Alden Addor <alden@aldens-air.lan>
2022-09-29 17:40:51 -0600

    Lab 12 continued - ascii image and qrcode generated.

--- code/alden/python/lab12.py
+++ code/alden/python/lab12.py
@@ -1,48 +1,80 @@
-# from cgi import test
-# from turtle import width
+import math
 from PIL import Image
+import qrcode
 
-ascii_characters = ['.',':','-','=','+','*','#','%','@']
-img = Image.open('test_image4.jpg').convert('L')
+# ascii_characters = ['.',':','-','=','+','*','#','%','@'] # pos full
+ascii_characters = [' ','.',':','-','=','*','#','%','@'] # pos with empty best with black background.
+# ascii_characters = ['@','%','#','*','+','=','-',':','.'] # neg full
+# ascii_characters = ['@','%','#','*','=','-',':','.',' '] # neg with empty
 
-        # Creating the aspect ratio to properly size the image.
-# def aspect_ratio(img):
-#     height, width = img.size
-#     asp_rat = height/width
-#     width = 120
-#     height = asp_rat * width * 0.55
-#     img = img.resize((width, int(height)))
-width, height = img.size
-asp_rat = height/width
-n_width = 120
-n_height = asp_rat * n_width * 0.55
-img = img.resize((n_width, int(n_height)))
+img = Image.open('image.jpg').convert('L')
 
-        # Changing the pixels into ascii characters
-def pixel():
+#---------------------------------------------------------------#
+    # Function to run through the conversion.
+
+def convert(img):
+    ratio = aspect_ratio(img)
+    new_pix = pixel(ratio)
+    ascii_img = formating(new_pix)
+    return save(ascii_img)
+
+
+#---------------------------------------------------------------#
+    # Creating the aspect ratio to properly size the image.
+
+new_width = 120
+def aspect_ratio(img):
+    width , height = img.size
+    r = math.gcd(width, height)
+    x = int(width / r)
+    y = int(height / r)
+    ratio = x/y
+    new_height = ratio * width * .166
+    img = img.resize((new_width, int(new_height)))
+    return img
+
+#---------------------------------------------------------------#
+    # Changing the pixels into ascii characters
+
+def pixel(img):
     pixels = img.getdata()
     pixel =[]
     for p in pixels:
         new_pix = ascii_characters[p//30]
         pixel.append(new_pix)
     new_pix = ''.join(pixel)
-# pixels = img.getdata()
-# pixel =[]
-# for p in pixels:
-#     new_pix = ascii_characters[p//30]
-#     pixel.append(new_pix)
-# new_pix = ''.join(pixel)
+    return new_pix
 
-# a= [p//25 for p in pixels]    Code used for how many ascii characters you need and for how they are selected.
+#---------------------------------------------------------------#
+    # Code used for how many ascii characters you need and for how they are selected.
+
+# pixels = img.getdata()
+# a= [p//25 for p in pixels]   
+# # print(a)
 # a = list(set(a))
 # print(a)
 
-def formating():
-    for i in range(0, len(pixel()), n_width):
+#---------------------------------------------------------------#
+    # Formats the string of new characters to show it as the image.
 
-   
+def formating(new_pix):
+    ascii_img=[]
+    for i in range(0, len(new_pix), new_width):
+        line = new_pix[i:i + new_width]
+        ascii_img.append(line)
+    ascii_img = '\n'.join(ascii_img)
+    return ascii_img
 
+#---------------------------------------------------------------#
+    # Saves to a .txt file.
 
-# print(new_pix)
+def save(ascii_img):
+    with open("image_pos_emp.txt", "w") as f:
+        f.write(ascii_img)
 
-# print(ascii_characters)
+#---------------------------------------------------------------#
+    # Running the program and conversion to a qrcode.
+convert(img)
+
+qr = qrcode.make("image_pos_emp.txt")
+qr.save("qrcode.png")
