509d525ef4fbd3b6409eae6bc3d7e48b2a108942
Jack Amoss <jackamoss@Jacks-MacBook-Pro.local>
2022-09-29 20:52:26 -0700

    keyword text targetting functional

--- code/jack/python/mcapstone/lab12.py
+++ code/jack/python/mcapstone/lab12.py
@@ -5,34 +5,34 @@ import statistics as s
 import string
 
 
-url = 'https://elavegan.com/easy-lentil-stew-mashed-potatoes-vegan/'
+url = 'https://www.allrecipes.com/recipe/15183/carrot-patties/'
 
-result = requests.get(url)
-doc = result.text # html
+def start(url):
+    result = requests.get(url)
+    doc = result.text # html
 
-soup = BeautifulSoup(doc, 'html.parser')
-all_text = soup.get_text().lower()
+    soup = BeautifulSoup(doc, 'html.parser')
+    all_text = soup.get_text().lower()
 
-file_title = soup.title.string
-file_title.replace(' ', '_')
+    file_title = soup.title.string.lower()
+    file_title = file_title.replace(' ', '_')
+    for x in range(len(file_title)):
+        if file_title[x] not in string.ascii_letters and file_title[x] != '_':
+            file_title = file_title[:x - 1]
+            break
 
-for x in range(len(file_title)):
-    print(x)
-    if file_title[x] not in string.ascii_letters or file_title[x] != '_':
-        file_title = file_title[:x - 1]
-print(file_title)
+    with open(f'./recipes/{file_title}.txt', 'w') as f: # save all text to file
+        f.write(f'url: {url}\n' + all_text)
+    
+    return all_text
+    # --- website body text can now be manipulated --- #
 
-# with open('storage.txt', 'w') as f: # save all text to file
-#     f.write(all_text)
-
-# --- website body text can now be manipulated --- #
-
-
+all_text = start(url) # gets text from webpage and saves file
 
 
 
 # POSSIBLE: could analyze many recipes to approximate words most often in recipes
-ingredient_keywords = ['ingredients', 'cup', 'tbsp', 'tablespoon', 'tsp', 'teaspoon'] # list of keywords commonly found in ingredient list
+ingredient_keywords = ['clove', 'pinch', 'pound', 'gram', 'ounce', 'ingredients', 'cup', 'tbsp', 'tablespoon', 'tsp', 'teaspoon'] # list of keywords commonly found in ingredient list
 
 
 
@@ -49,28 +49,47 @@ keyword_indices = []
 for x in ingredient_keywords: # populates list with all indices of found keywords
     keyword_indices += (find_indices(x, all_text))
 keyword_indices = sorted(keyword_indices)
-# print(keyword_indices)
+print(keyword_indices)
 
 keyword_indices_variance = []
 for x in range(len(keyword_indices) - 1): # populates list with index variance from next index, representing how condensed keywords are
     variance = keyword_indices[x + 1] - keyword_indices[x]
     keyword_indices_variance.append(variance)
-# print(keyword_indices_variance)
+print(keyword_indices_variance)
 
 keyword_indices_variance_select = []
 for x in range(len(keyword_indices_variance)): # populates list with indices of hits in close proximity of eachother - indices to be used with keyword_indices list
     if keyword_indices_variance[x] < int(round(s.mean(keyword_indices_variance))):
         keyword_indices_variance_select.append(x)
-# print(keyword_indices_variance_select)
+print(keyword_indices_variance_select)
 
 keyword_indices_select = []
 for x in keyword_indices_variance_select: # populates list with indices of keywords expected to be in recipe
     keyword_indices_select.append(keyword_indices[x])
+
+
 print(keyword_indices_select)
 
+def largest_sequence(my_list):
+    temp = []
+    for x in range(len(my_list) - 1):
+        if my_list[x] + 1 == my_list[x + 1] + 1:
+            temp.append(my_list[x])
+
+
+ingredients = all_text[keyword_indices_select[0]: keyword_indices_select[-1]]
 
 
 
+
+# --------- TESTING ---------------------------------------------------------------------------------------------------
+
+
+
+
+
+with open(f'./ingredients/temp.txt', 'w') as f: # save ingredients to file
+        f.write(ingredients)
 
 # --------- RECYCLING BIN ---------------------------------------------------------------------------------------------------
 
