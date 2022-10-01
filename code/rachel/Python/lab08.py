de36a16077beecd8dbea6492514aaa35557a422d
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2022-09-30 20:33:30 -0400

    Lab 8 Completed and Updated

--- code/rachel/Python/lab08.py
+++ code/rachel/Python/lab08.py
@@ -4,6 +4,7 @@ with open('contacts.csv', 'r') as file:
     lines = file.read().split('\n')
     new_lists = [line.split(',') for line in lines] #turn those strings into their own list of things to work with: [['name', 'favorite color', 'favorite fruit'], ['bob', 'green', 'cherry'], ['julia', 'yellow', 'pineapple'], ['manny', 'red', 'apple'], ['debby', 'purple', 'kiwi']]
     #print(new_lists)
+headers = []
 headers = new_lists.pop(0) #pop out first list
 #print(headers) #['name', 'favorite color', 'favorite fruit']
 #print(new_lists) #[['bob', 'green', 'cherry'], ['julia', 'yellow', 'pineapple'], ['manny', 'red', 'apple'], ['debby', 'purple', 'kiwi']]
@@ -17,50 +18,54 @@ def list_builder(funny, new_lists): #parameter names only apply to the function 
 #print(list_builder(headers, new_lists)) #this is where I tell it to use the particular lists I want it to use
 contact_list = list_builder(headers, new_lists)
 #print(contact_list)
+def get_record(contact_list, headers):
+    user_name = input("Enter the user's name ")
+    for contact in contact_list:
+        if contact['name'] == user_name:
+            for header in contact:
+                # print(f'{header}: {contact[header]}')
+                return contact
 """Version 2: Implement a CRUD REPL: 
 Create a record: ask the user for each attribute, add a new contact to your contact list with the attributes that the user entered."""
-decision1 = input('To enter your contact information, enter "new user". If you already have entered your information, enter "verify" to verify your information or "delete" to erase your information ')
-if decision1 == 'new user':
-    #local_list = list_builder()
-    user_name = input("Enter your first name: ")
-    fav_color = input("Enter your favorite color: ")
-    fav_fruit = input("Enter your favorite fruit: ")
-    new_user = {
-        'name': user_name,
-        'favorite color': fav_color,
-        'favorite fruit': fav_fruit
-        }
-    contact_list.append(new_user)
-    print(new_user)
+while True:
+    decision1 = input('Enter an option:\n To enter your contact information, enter "new user".\n To verify your information enter "verify".\n To update your record, enter "update".\n To delete your information, enter "delete".\n To quit the program, enter "quit". ')
+    if decision1 == 'new user':
+        user_name = input("Enter your first name: ")
+        fav_color = input("Enter your favorite color: ")
+        fav_fruit = input("Enter your favorite fruit: ")
+        new_user = {
+            'name': user_name,
+            'favorite color': fav_color,
+            'favorite fruit': fav_fruit
+            }
+        contact_list.append(new_user)
+        print(new_user)
     #print(contact_list)
     """Retrieve a record: ask the user for the contact's name, find the user with the given name, and display their information"""
-elif decision1 == 'verify':
-    user_name = input('Please verify your information. What is your first name? ')
-    match = next((l for l in contact_list if l['name'] == user_name)) #match the user_name to the name value and return the entire dictionary the name was found in
-    print(match)
+    if decision1 == 'verify':
+        user_name = input('To verify your information, enter your first name ')
+        for contact in contact_list: #for each list in the file
+            if contact['name'] == user_name: #find the name in the file
+                for header in contact: 
+                    print(f'{header}: {contact[header]}') #print out all the key value pairs in that list
     """Update a record: ask the user for the contact's name, then for which attribute of the user they'd like to update and the value of the attribute they'd like to set."""
-    decision2 = input('If your information is correct enter "quit", and if not, enter "update" ')
-    if decision2 == 'update':
-    #while decision2 == 'update':
-        update_key = input('Enter the field that needs to be updated ')
-        update_value = input('Enter the correct information ')
-        if update_key == 'name':
-            match['name'] = update_value
-        elif update_key == 'favorite color':
-            match['favorite color'] = update_value
-        elif update_key == 'favorite fruit':
-            match['favorite fruit'] = update_value
-        print(match)
-        contact_list.append(match)
+    if decision1 == 'update':
+        found_contact = get_record(contact_list, headers) #run function
+        update_key = input(f'Which field do you want to update? {headers}') #ask user which key they want to update and provide the keys so they know what to choose
+        update_value = input(f'Enter the correct information for {update_key}: ') #ask user what they want to change the value for that key to
+        found_contact[update_key] = update_value #update the record 
+        #contact_list.append(match)
         #print(contact_list)
     """Delete a record: ask the user for the contact's name, remove the contact with the given name from the contact list."""
-elif decision1 == 'delete':
-    user_name = input('What is the first name of the record you want to delete? ')
-    for i in range(len(contact_list)):
-        if contact_list[i]['name'] == user_name:
-            del contact_list[i]
-            break
-    print('Your record has been deleted')
+    if decision1 == 'delete':
+        user_name = input('What is the first name of the record you want to delete? ')
+        for i in range(len(contact_list)): #another way to specify each list in the file
+            if contact_list[i]['name'] == user_name: #find the list that contains the user's name
+                del contact_list[i] #delete the corresponding record
+        print('Your record has been deleted')
+    if decision1 == 'quit':
+        print('Thanks for visiting')
+        break
     #print(contact_list)           
 with open ('contacts.csv', 'w') as csvfile:
     writer = csv.DictWriter (csvfile, fieldnames=headers)
