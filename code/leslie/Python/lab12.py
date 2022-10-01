7555fdde29e92a7af84494c9d1dd239c671bbe93
Leslie <leslie.l.naylor@gmail.com>
2022-10-01 01:49:45 -0400

    lab12 - will be starting over

--- code/leslie/Python/lab12.py
+++ code/leslie/Python/lab12.py
@@ -1,83 +1,70 @@
 import tkinter as tk
-
+from tkinter import * #includes messagebox
 import time
 
-total_time = 0 #input("How many seconds? ")
-def countdown(total_time):
-    while total_time > 0:
-        mins = total_time // 60
-        secs = total_time % 60
-        total_timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(total_timer, end="\r") # basically overwrites current line-- makes next line begin at current line
+class Pomodoro:
+    def __init__(self, root):
+        self.root = root
+        
+        
+    def _time(self, timer):
+        #how to display minutes & seconds on GUI
+        minutes, seconds = divmod(timer, 60) #divmod(x,y) returns values of x/y as (quotient, remainder) divmod(9,3) // (3,0)
+        self.min.set(f"{minutes:02d}")
+        self.sec.set(f"{seconds:02d}")
+        self.root.update()
         time.sleep(1)
-        total_time -= 1
-    print("Done!")
-#countdown(int(total_time))
-
-def pomodoro():    
-    for i in range(4):
-        print("Time to Work!")
-        total_time = 5 #25 minutes * 60 seconds to get total seconds
-        while total_time > 0:
-            mins = total_time // 60
-            secs = total_time % 60
-            total_timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(total_timer, end="\r") # basically overwrites current line-- makes next line begin at current line
-            time.sleep(1)
-            total_time -= 1
-        print("Time for a Break!")
-
-        total_time = 5
-        while total_time > 0:
-            mins = total_time // 60
-            secs = total_time % 60
-            total_timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(total_timer, end="\r") # basically overwrites current line-- makes next line begin at current line
-            time.sleep(1)
-            total_time -= 1
-        print("Break's Over!")
-#pomodoro() 
-
-""" def start_pause():
-    if start_button['text'] == 'Start':
-        pomodoro()
-        start_button['text'] = 'Pause'
-    else:
-        start_button["text"]= "Start" """
+        
+    def work_time(self):  
+        timer = 25*60 #25 minutes * 60 seconds to get total seconds   
+        while timer > 0:
+            pomo._time(timer)
+            if timer == 0:
+                print("Time for your break!")
+            timer -=1
+    
     
 
 #----------UI-------------------------#
+    def main(self):
+        self.root = Tk()
+        self.root.geometry("365x365")       
+        self.root.title("Pomodoro Timer") 
+        #self.window = tk.Tk() #Creates window, but if I don't explicitly initialize it, one will be implicitly created when I create widget. But show it anyway.
+        
+        self.label = tk.Label(                  #Create label
+            text="Pomodoro Timer",
+            fg="white",
+            bg="green",
+            width=25,
+            height=2,
+            
+        )
+        self.label.pack() #Smacks label on window
 
-window = tk.Tk() #Creates window
-frame = tk.Frame(master=window, borderwidth=5)
-frame.pack()
-label = tk.Label(                  #Create label
-    text="Pomodoro Timer",
-    fg="white",
-    bg="green",
-    width=25,
-    height=2
-)
-label.pack() #Smacks label on window
-
-
-
-start_button = tk.Button(
-    text="Start Timer",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="white",
-    relief="groove",
-    command=pomodoro
-)
-start_button.pack()
+        self.start_button = tk.Button(
+            text="Start Timer",
+            width=25,
+            height=5,
+            bg="blue",
+            fg="white",
+            relief="groove",
+            command=Pomodoro
+        )
+        self.start_button.pack()
+        
+         
 
 
 
 
 
-window.mainloop() #CALL LAST -- after creating widgets! Keeps window visible on screen until I close it
+
+
+        self.root.mainloop() #CALL LAST -- after creating widgets! Keeps window visible on screen until I close it
 #Blocks any code that comes after it until 
-#Pomodoro total_timer
+#Pomodoro timerr
 #25 minute work, 5 minute break on loop      
+if __name__ == '__main__':
+    pomo = Pomodoro(tk.Tk())
+    pomo.main()
