33a3d106a0f2ecdce4952c03b54d0294355eec7b
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-10-03 16:21:11 -0700

    Billy Frick - Lab 12 (Completed)

--- code/billy/fighter_characters.py
+++ code/billy/fighter_characters.py
@@ -1,31 +1,196 @@
 import pygame
-
 class character_models():
-    def __init__(self, x, y):
-        self.rect = pygame.Rect((x, y, 30, 30))
+    def __init__(self, player, x, y, flip,  data, sprite_sheet, animation_frames, sound):
+        self.player = player
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.animation_list = self.load_images(sprite_sheet, animation_frames) # takes animation steps and adds it to a list.
+        self.action = 0 # determines animations that will change, depending on what the character is doing.
+        self.frame_index = 0 # starts animations at the first frame.
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time = pygame.time.get_ticks()
+        self.flip = flip
+        self.rect = pygame.Rect((x, y, 80, 180)) 
+        self.vel_y = 0 # starting y (jump) velocity.
+        self.running = False
+        self.jump = False # defaults the character to not be in a jumping state.
+        self.attacking = False # defualts character to not attack.
+        self.attack_type = 0 # sets default attack type to 0.
+        self.attack_cooldown = 0
+        self.health = 100 # both characters start with 150 HP.
+        self.damage = False
+        self.alive = True
+        self.attack_sound = sound
 
-    def character_movement(self, screen_width):
+    def load_images(self, sprite_sheet, animation_frames):
+        '''extracts images from the sprite sheets.'''
+        animation_list = []
 
-        SPEED = 10
-        dx = 0
+        for y, animations in enumerate(animation_frames):
+            temporary_image_list = []
+
+            for x in range(animations):
+                temporary_image = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size) # extracts individual rows for sprite_sheets. # produces a finalized temporary image list. It will contain 1 rows worth of images.
+                temporary_image_list.append(pygame.transform.scale(temporary_image, (self.size * self.image_scale, self.size * self.image_scale))) 
+            animation_list.append(temporary_image_list) # adds to the master animation list and goes to the next row.
+        return animation_list
+
+    def character_movement(self, screen_width, screen_height, surface, target):
+
+        SPEED = 7 # character's speed
+        GRAVITY = 2 # character's gravity
+        dx = 0 
         dy = 0
+        self.attack_type = 0 # starts character without an attack.
+        self.running = False # starts running animation as false, and resets to false when no longer running.
 
-        key = pygame.key.get_pressed()
+        '''characters x and y movements + key bindings.'''
+        key = pygame.key.get_pressed() # gets a keypress 
 
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = SPEED
+        if self.attacking == False and self.alive == True: # the following can only be done if the character is not attacking.
 
-        if self.rect.left + dx < 0:
+            if self.player == 1:
+                if key[pygame.K_a]: # if a on the keyboard is pressed.
+                    dx = -SPEED # SPEED to the left.
+                    self.running = True
+                if key[pygame.K_d]: # if d on the keyboard is pressed.
+                    dx = SPEED # SPEED to the right.
+                    self.running = True
+
+                '''character jump'''
+                if key[pygame.K_w] and self.jump == False: # this will initiate a character jump, if they are not already jumping.
+                    self.vel_y = -30 # negative value will move the character upwards.
+                    self.jump = True
+
+                '''character attack + key bindings.'''
+                if key[pygame.K_SPACE] or key[pygame.K_r]: # assigns attacks to keys:
+
+                    self.attacks(target)
+
+                    if key[pygame.K_SPACE]:
+                        self.attack_type = 1 # assigns attack 1 to the spacebar.
+                    if key[pygame.K_r]:
+                        self.attack_type = 2 # assigns attack 2 to the r key.
+
+            if self.player == 2:
+                if key[pygame.K_LEFT]: # if a on the keyboard is pressed.
+                    dx = -SPEED # SPEED to the left.
+                    self.running = True
+                if key[pygame.K_RIGHT]: # if d on the keyboard is pressed.
+                    dx = SPEED # SPEED to the right.
+                    self.running = True
+
+                '''character jump'''
+                if key[pygame.K_UP] and self.jump == False: # this will initiate a character jump, if they are not already jumping.
+                    self.vel_y = -30 # negative value will move the character upwards.
+                    self.jump = True
+
+                '''character attack + key bindings.'''
+                if key[pygame.K_KP0] or key[pygame.K_KP3]: # assigns attacks to keys:
+                    self.attacks(target)
+                    if key[pygame.K_KP0]:
+                        self.attack_type = 1 # assigns attack 1 to the spacebar.
+                    if key[pygame.K_KP3]:
+                        self.attack_type = 2
+
+
+
+        '''character's y velocity will be 'brought down' by GRAVITY.'''
+        self.vel_y += GRAVITY 
+        dy += self.vel_y
+
+
+        ''' ensures the character will stay on the screen.'''
+        if self.rect.left + dx < 0: # 
             dx = -self.rect.left
         if self.rect.right + dx > screen_width:
             dx = screen_width - self.rect.right
+        if self.rect.bottom + dy > screen_height - 30:
+            self.vel_y = 0 # stops character
+            self.jump = False # resets the jump to False when the character touches the ground.
+            dy = screen_height - 30 - self.rect.bottom # the difference between the 'floor' and character (in pixels).
 
-        
-        self.rect.x += dx
-        self.rect.y += dy
+        '''ensures characters will face each other.'''
+        if target.rect.centerx > self.rect.centerx: # flip stays False, as the characters are already facing each other.
+            self.flip = False
+        else:
+            self.flip = True # faces character towards the target.
+
+        '''attack animation cooldown'''
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
+            
+        '''updates the characters position.'''
+        self.rect.x += dx # characters position is changed by dx variable
+        self.rect.y += dy # characters position is changed by the dy variable.
+
+    def update_animation(self):
+        if self.health <= 0:
+            self.health = 0
+            self.alive = False
+            self.update_actions(6) # death animation.
+        elif self.damage == True:
+            self.update_actions(5) # flinch animation.
+        elif self.attacking == True:
+            if self.attack_type == 1:
+                self.update_actions(3) # 1st attack animation.
+            elif self.attack_type == 2:
+                self.update_actions(4) # 2nd attack animation.
+        elif self.jump == True:
+            self.update_actions(2) # jumping animation.
+        elif self.running == True:
+            self.update_actions(1) # running animation.
+        else:
+            self.update_actions(0) # idle animation.
+
+
+        '''handles animation updates, and displays on screen.'''
+        animation_cooldown = 50 # determines cooldown on frames.
+        self.image = self.animation_list[self.action][self.frame_index] # increased frame index and allows animation to run.
+
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown: # checks if enough time has passed since the last animation.
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animation_list[self.action]): # loops animations
+            if self.alive == False:
+                self.frame_index = len(self.animation_list[self.action]) - 1
+            else:
+                self.frame_index = 0
+                if self.action == 3 or self.action == 4:
+                    self.attacking = False
+                    self.attack_cooldown = 27
+            if self.action == 5: # returns character to idle animation after taking damage.
+                self.hit == False 
+                self.attacking = False # cancels out character attacks, if damaged.
+                self.attack_cooldown = 27
+
+    '''
+    creates the hitbox for attacks. x and y coordinates are based on the character attacking. 
+    centerx will throw the attack from the middle of the character model.
+    2 * will make the attack 2 times as wide as the character currently is.
+    '''
+    def attacks(self, target):
+
+        if self.attack_cooldown == 0:
+            self.attacking = True
+            self.attack_sound.play()
+            attacking_hitbox = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height) # self.flip will offsets hitbox from the right, to the left.
+
+            if attacking_hitbox.colliderect(target.rect): # checks for collision between two different 'rectangles.'
+                target.health -= 13 # the target will lose 13 HP for every hit.
+                target.hit = True
+            
+
+    def update_actions(self, new_animation):
+        '''compares next animation to see if it is different than the previous one.'''
+        if new_animation != self.action:
+            self.action = new_animation
+
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
 
     def display_character(self, surface):
 
-        pygame.draw.rect(surface, (0,255,0), self.rect)
+        image = pygame.transform.flip(self.image, self.flip, False) # re-directs character to face the opponent.    
+        surface.blit(image, (self.rect.x - (self.offset[0] * self.image_scale) , self.rect.y - (self.offset[1] * self.image_scale)))
--- code/billy/lab12_mini_capstone.py
+++ code/billy/lab12_mini_capstone.py
@@ -1,46 +1,122 @@
-
 import pygame
+from pygame import mixer
 from fighter_characters import character_models
 from numpy import character
 
+mixer.init()
 pygame.init()
 
-pygame.display.set_caption('Cosmic Chaos')
+pygame.display.set_caption('Krusty Krab Chaos')
 
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN_HEIGHT = 600
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # loads background image.
+
+'''sets frames per second'''
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 120
 
-background_image = pygame.image.load('space_background.jpg').convert_alpha()
+'''define colors'''
+WHITE = (255, 255, 255)
+GREEN = (0, 150, 0)
+RED = (230, 0, 0)
+
+score = [0, 0]
+end_of_round = False
+ROUND_COOLDOWN = 5000
+RESET = 3000
+
+death_bringer_sheet = pygame.image.load('wizard.png').convert_alpha() # loads the death bringer sprite sheet.
+angel_sheet = pygame.image.load('warrior.png').convert_alpha() # loads the angel sprite sheet.
+
+DEATH_BRINGER_ANIMATIONS = [8, 8, 1, 8, 8, 3, 7]  # defines the  number of steps for each animation (numbers equal animation frames).
+ANGEL_ANIMATIONS = [10, 8, 1, 7, 7, 3, 7] # defines the number of steps for each animation (numbers equal animation frames).
+
+'''define death bringer variables'''
+DEATH_BRINGER_SIZE = 250  # png size divided by number of items in the row.
+DEATH_BRINGER_SCALE = 3
+DEATH_BRINGER_POSITION = [112, 107] #offsets to align with their rect.
+DEATH_BRINGER_DATA = [DEATH_BRINGER_SIZE, DEATH_BRINGER_SCALE, DEATH_BRINGER_POSITION]
+'''define angel variables'''
+ANGEL_SIZE = 162
+ANGEL_SCALE = 4
+ANGEL_POSITION = [72, 56] # offsets to align with rect.
+ANGEL_DATA = [ANGEL_SIZE, ANGEL_SCALE, ANGEL_POSITION]
+
+'''load music and sound.'''
+pygame.mixer.music.load('rick_roll.mp3')
+pygame.mixer.music.set_volume(0.75)
+pygame.mixer.music.play(-2, 0.0, 5000)
+
+sword_fx = pygame.mixer.Sound('sword_effect.wav')
+sword_fx.set_volume(0.17)
+
+magic_fx = pygame.mixer.Sound('magic_effect.wav')
+magic_fx.set_volume(1)
+
+background_image = pygame.image.load('krusty_krab.jpg').convert_alpha() # sets the background image
 
 def background_rendering():
-
-    scaled_background = pygame.transform.scale(background_image,(SCREEN_WIDTH, SCREEN_HEIGHT))
+    '''renders the background and sets the screen width and height.'''
+    scaled_background = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_background,(0,0))
 
-character_1 = character_models(200, 400)
-character_2 = character_models(700, 400)
+def healthbar_rendering(health, x, y):
+    '''renders the health bars on screen and will reduce healthbar size to show health loss.'''
+    ratio = health / 100 # divides healthbar by the ratio.
+    pygame.draw.rect(screen, WHITE,(x - 2, y - 2, 304, 24 ))
+    pygame.draw.rect(screen, RED, (x, y, 300, 20)) # sits behind healthbar and will display "health loss."
+    pygame.draw.rect(screen, GREEN, (x, y, 300 * ratio, 20)) # assigns health bars to the screen, location and width/height.
+    
+
+
+'''creates instances of each character''' 
+character_1 = character_models(1, 200, 150, False, ANGEL_DATA, angel_sheet, ANGEL_ANIMATIONS, sword_fx)
+character_2 = character_models(2, 700, 150, True, DEATH_BRINGER_DATA, death_bringer_sheet, DEATH_BRINGER_ANIMATIONS, magic_fx)
 
 game_running = True
 
+'''main game loop'''
 while game_running:
 
     clock.tick(FPS)
 
     background_rendering()
 
-    character_1.character_movement(SCREEN_WIDTH)
-    # character_2.character_movement()
+    healthbar_rendering(character_1.health, 20, 20) # displays the character status (health) and the exact x/y location.
+    healthbar_rendering(character_2.health, 680, 20)
+
+    character_1.character_movement(SCREEN_WIDTH, SCREEN_HEIGHT, screen, character_2, ) # character_2 argument allows charcter 1 to attack them.
+    character_2.character_movement(SCREEN_WIDTH, SCREEN_HEIGHT, screen, character_1)
+
+    '''updates character animations'''
+    character_1.update_animation() 
+    character_2.update_animation()
 
 
-    character_1.display_character(screen)
+    '''renders the character on the screen.'''
+    character_1.display_character(screen) 
     character_2.display_character(screen)
 
-    for event in pygame.event.get():
+    if end_of_round == False:
+        if character_1.alive == False:
+            score[1] += 1
+            end_of_round = True
+            RESET = pygame.time.get_ticks()
+            print(score)
+        elif character_2.alive == False:
+            score[0] += 1
+            end_of_round = True
+            RESET = pygame.time.get_ticks()
+            print(score)
+    else:
+        if pygame.time.get_ticks() - RESET > ROUND_COOLDOWN:
+            end_of_round = False
+            character_1 = character_models(1, 200, 310, False, ANGEL_DATA, angel_sheet, ANGEL_ANIMATIONS, sword_fx)
+            character_2 = character_models(2, 700, 310, True, DEATH_BRINGER_DATA, death_bringer_sheet, DEATH_BRINGER_ANIMATIONS, magic_fx)
 
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
 
