72cccb3fe13d8d2d60fb6cf2b649c4d6798b0be4
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-10-04 01:06:21 -0400

    Lab12 Finished

--- code/sarde/python/lab12.py
+++ code/sarde/python/lab12.py
@@ -11,87 +11,56 @@
 -and BMI would be calculated from the values entered
 -BMI = Body Mass Index
 -BMI = (weight in pounds * 703) / (height in inches * height in inches)
--Height in inches
-    -Example: 5ft 3in = 12in = 1ft, 5*12=60 +3in = 63in
+-Height-entered in inches
 '''
-# external library --> Tkinter --> pip install tk
-# Tkinter --> tea-kay-inter
-# python -m tkinter --> open a window --> Tk interface
-# create an instance of the tk.Tk class --> create application window
-# main window = root --> can use any name
+# --> TKINTER
 import os
 import tkinter as tk
 from tkinter import ttk
-from turtle import fillcolor
-main_window = tk.Tk()  # --> object
-greeting = tk.Label(main_window, text="Hello, Sarde!")
-greeting.pack()
-# --> change window title
-main_window.title('BMI Calculator')  # --> window title
+from tkinter import *
+from PIL import Image, ImageTk
+main_window = Tk()  # GUI --> an object
+
 # --> change the size and position of window
-# main_window.geometry('300x200')
-main_window.geometry('300x200+40+40')
+main_window.geometry("350x350+40+40")
+# window-title
+main_window.title('BMI Calculator')
 
-'''
-ICON PHOTO --> not working
-# icon_photo = tk.PhotoImage(file='icons8-bmi-80.png')
-# main_window.iconphoto(False, icon_photo)
-main_window.iconbitmap(os.path.dirname(
-    os.path.abspath(__file__))+'/icons8-bmi-30.ico')
-'''
+# --> icon photo
+main_window.iconbitmap("tk-0.1.0/icons8-heart-health-50.png")
 
 
-def calculate_bmi(weight, height):
-    # calculate BMi with the weight and heigth entered
-    weight = input('Weight: ')
-    height = input('Height: ')
-    weight = int(weight)
-    height = int(height)
-    return (weight * 703) / (height * height)
+# image
+image = Image.open("tk-0.1.0/balloon_bmi.jpg")
+img = image.resize((275, 175))  # --> resized image
+photo = ImageTk.PhotoImage(img)
+img_label = Label(image=photo,)
+img_label.pack()
 
 
-# frame
-frame = ttk.Frame(main_window)
-
-# weight label
-weight_label = ttk.Label(main_window, text='Weight(lbs)')
-weight_label.pack()
-
-# weight entry
-weight = tk.StringVar()
-weight_entry = ttk.Entry(main_window, textvariable=weight)
-weight_entry.pack()
-
-# height label
-height_label = ttk.Label(main_window, text='Height(in)')
-height_label.pack()
-
-# height_entry
-height = tk.StringVar()
-height_entry = ttk.Entry(main_window, textvariable=height)
-height_entry.pack()
-
-# calculate BMI button
+def calculate_bmi():
+    # calculate BMI with the weight and heigth entered
+    weight = int(w.get())  # --> gets the output of an entry widget
+    height = int(h.get())
+    bmi = round((weight * 703) / (height * height), 1)
+    label.config(text=bmi)
 
 
-def calculate_bmi_button_clicked():
-    # calculate bmi button-click event
-    bmi = calculate_bmi(weight, height)
-    result = f'Your BMI is {bmi} '
-    return result
+# weigth widget- user will enter weight
+w_label = Label(main_window, text='Weight(lbs)', font=('Calibri 13')).pack()
+w = Entry(main_window, width=30)
+w.pack()  # --> arranges widgets around the edges of their container(root window, frame)
+
+# heigth widget-user will enter height
+h_label = Label(main_window, text='Height(in)', font=('Calibri 13')).pack()
+h = Entry(main_window, width=30)
+h.pack()
+
+label = Label(main_window, text="Your BMI is: ",
+              fg='magenta', font=('Calibri 15'))
+label.pack(pady=10)
 
 
-calculate_bmi_button = ttk.Button(main_window, text='Calculate BMI')
-calculate_bmi_button.pack()
-calculate_bmi_button.configure(command=calculate_bmi_button_clicked)
-# calculate_bmi_button.pack()
-
-# seperator
-seperator = ttk.Separator(main_window, orient='horizontal')
-seperator.pack(fill='x')
-
-# return calculated BMI to user
-
-
-# call mainloop method --> keeps the window visible on the screen, until ypu close it
+Button(main_window, text="Calculate BMI",
+       bg="magenta", command=calculate_bmi).pack()
 main_window.mainloop()
