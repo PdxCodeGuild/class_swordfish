067d70713360bb85606931b196a54e4040e4282e
Jose Cortes <jcortes8989@gmail.com>
2022-10-10 16:22:26 -0700

    HMTL Labs in progress

--- code/jose/HTML_labs/webpagelab03/webpage.py
+++ code/jose/HTML_labs/webpagelab03/webpage.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    posts = [ {'title': 'Yui Reopens',
+    posts = [ {'title': 'Yui, a Thai Restaurant',
     'author': 'Katherine Chew Hamilton',
     'date': 'March 3rd, 2022',
     'body': ''
@@ -16,12 +16,12 @@ def index():
     'date': '2022',
     'body': 'An cocktail bar located in Northeast Portland, this bar may not have food but the location is amazing. THey are connected together with four restaurants in a small complex called "The Zipper". You can get a very spciy fried chicken sandwich from Basilisk, amazing Mexican food from Tight Tacos, delicious Korean fried chicken from Sari Ramyun, and classic pizza from Boxcar Pizza.',
 },{
-    'title': 'Inside Division Cocktail Bar Someday, Opening Today',
+    'title': 'Someday, a cocktail lounge',
     'author': 'Alex Frane',
     'date':'January 2nd, 2020' ,
     'body':'Someday bar is locaed on fabulous Division St. in Southeast. The restaurant shares its space with three other food carts in their cozy backalley patio space.',
 },{
-    'title': 'Deadshot PDX: The Most Unique Cocktails in Portland',
+    'title': 'Deadshot, a unique cocktail bar and restaurant',
     'author': 'Sean',
     'date': 'February 2nd, 2020',
     'body':'A unique cocktail bar with fantastic food! THe drinks are upper tier quality out of all of the cocktail lounges in Portland. Definitely a must try!' ,
