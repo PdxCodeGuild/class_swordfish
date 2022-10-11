64b27070e3b67b9c5ae493bba32f30df6ab9f516
Leslie <leslie.l.naylor@gmail.com>
2022-10-11 18:00:23 -0400

    in progress

--- code/Group_Lab_1/lab_test.py
+++ code/Group_Lab_1/lab_test.py
@@ -4,12 +4,41 @@ from jsondb import JsonDB
 app = Flask(__name__)
 
 @app.route('/')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     db = JsonDB('db.json')
     db.load()
     x = db.get('todos', 0)
+    if request.method == 'POST':
+        db.load()
+        todo_text = request.form['input_text']
+        print("to do:", todo_text)
+        priority = request.form['priority']
+        print(priority)
+        todo_list = []
+        todos = {}
+        todos['text'] = todo_text
+        todos['priority'] = priority
+        todo_list.append(todos)
+        db.set('todos', todo_list)
+        
+        
+        
+        db.save()
+        
+        
+        
+        return redirect('/')
+    
+    
     the_string = 'the other world'
     return render_template('index.html', the_string=the_string, x=x)
-
-
+    
+        
+        
+    # db = JsonDB('db.json')
+    # db.set('text', 'input_text')
+    # db.save()
 app.run(debug=True)
