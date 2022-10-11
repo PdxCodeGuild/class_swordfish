653fee0e0209497a8dba0e8300622043e81f23c6
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-10-11 11:57:38 -0700

    billy frick - pushed changes to group lab (to-do list).

--- code/Group_3/app.py
+++ code/Group_3/app.py
@@ -2,11 +2,26 @@
 A simple JSON-based database that can be used with Flask.
 Usage:
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from jsondb import JsonDB
-db = JsonDB('db.json')
-db.load()
-x = db.get('todos', 0)
-x.append(x[0])
-db.set('todos', x)
-db.save()
+# x = db.get('todos', 0)
+# x.append(x[0])
+# db.set('todos', x)
+# db.save()
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    db = JsonDB('db.json')
+    db.load()
+    todos = db.get('todos')
+
+    if request.method=='POST':
+      text = request.form['text']
+      text.append(text[0])
+      db.set('todos', text)
+      db.save() 
+      return redirect('/')
+    return render_template('index.html', todos = todos)
+
+app.run(debug=True)
