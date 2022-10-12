2587518bd23e874b2bd522c24befda048890c863
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-10-12 17:26:51 -0400

    Lab03 Finished

--- code/sarde/Flask_HTML_CSS/lab03/app.py
+++ code/sarde/Flask_HTML_CSS/lab03/app.py
@@ -6,25 +6,25 @@ app = Flask(__name__)
 # Create a list of at least four dictionaries for your blog posts
 posts = [
     {
-        'title': "Welcome!",
+        'title': "Hello & Welcome",
         'author': "Sarde Essilfie",
         'date': "October 6th, 2022",
         'body': "Buckeye Ipsum woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus."
     },
     {
-        'title': "Something",
+        'title': "Upcoming Apple Products: M2 Pro and M2 Max chips-Macbook Pro.",
         'author': "Sarde Essilfie",
         'date': "October 7th, 2022",
         'body': "RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence."
     },
     {
-        'title': "Something",
+        'title': "Tech For Travel: Apple Airtags",
         'author': "Sarde Essilfie",
         'date': "October 8th, 2022",
         'body': "Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio."
     },
     {
-        'title': "Something",
+        'title': "Code With Me",
         'author': "Sarde Essilfie",
         'date': "October 9th, 2022",
         'body': "SMorrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state the union tbdbitl OH-IO script ohio carmen ohio Hagerty hall Fisher John Glenn buckeyes Lee Horvath Michael Redd the Lantern Morrill Hayes st. john's excellence Urban Meyer Hoppalong Cassidy Woody Hayes Columbus Orlando Pace 14-0 Olentangy river Les Wexner Mike Conley Ryan Shazier Eddie George Dr. Michael Drake RPAC Skull Session 1870 Jack Nicklaus woody Archie Griffin WOSU oval mirror lake horseshoe moritz scarlet gray Brutus buckeye leaf ohio state."
