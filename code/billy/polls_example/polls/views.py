29437ee9d4611da7ecdf127de65fdb1c17c77a21
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-10-19 16:26:16 -0700

    Billy Frick - pushed all work done for 'polls_example'.

--- code/billy/polls_example/polls/views.py
+++ code/billy/polls_example/polls/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
 
 from.models import Question, Choice
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:10]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:10]
     return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 
@@ -13,7 +15,17 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse(f"Your looking at the results of question number {question_id}.")
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 def vote(request, question_id):
-    return HttpResponse(f"You are voting on question number {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice = question.choices.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question, 
+            'error_message': "please select a valid choice."
+        })
+    choice.votes += 1
+    choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
