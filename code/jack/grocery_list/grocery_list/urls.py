4d0e2cc98e3580653df4bbc79cd9f9a9853e9382
jackamoss2 <112007713+jackamoss2@users.noreply.github.com>
2022-10-21 17:18:57 -0700

    Grocery List lab done

--- code/jack/grocery_list/grocery_list/urls.py
+++ code/jack/grocery_list/grocery_list/urls.py
@@ -5,5 +5,7 @@ app_name = 'grocery_list'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
-    path('complete/', views.complete, name='complete'),
+    path('<int:item_id>/complete', views.complete, name='complete'),
+    path('<int:item_id>/incomplete', views.incomplete, name='incomplete'),
+    path('<int:item_id>/delete', views.delete, name='delete'),
 ]
--- code/jack/grocery_list/grocery_list/views.py
+++ code/jack/grocery_list/grocery_list/views.py
@@ -10,17 +10,6 @@ from .models import GroceryItem
 def index(request):
     incomplete_items = GroceryItem.objects.filter(complete=False)
     completed_items = GroceryItem.objects.filter(complete=True)
-    #     if request.method=='POST':
-
-    #     else: # if not creating new item
-    #         for item in incomplete_items:
-    #             if str(item.id) + ' complete' in request.POST:
-    #                 item.complete = True
-    #                 item.complete_date = datetime.now()
-    #                 item.save()
-    #             elif str(item.id) + ' delete' in request.POST:
-    #                 item.delete()
-    #         incomplete_items = GroceryItem.objects.filter(complete=False) # update list
     return render(request, 'grocery_list/index.html', {'incomplete_items':incomplete_items, 'completed_items':completed_items})
 
 def add(request):
@@ -33,12 +22,22 @@ def add(request):
     return HttpResponseRedirect("../")
 
 def complete(request, item_id):
-    item = get_object_or_404(GroceryItem.objects.get(id=item_id))
+    item = get_object_or_404(GroceryItem.objects.filter(id=item_id))
     item.complete = True
     item.complete_date = datetime.now()
     item.save()
     return HttpResponseRedirect("../")
 
+def incomplete(request, item_id):
+    item = get_object_or_404(GroceryItem.objects.filter(id=item_id))
+    item.complete = False
+    item.complete_date = None
+    item.save()
+    return HttpResponseRedirect("../")
 
+def delete(request, item_id):
+    item = get_object_or_404(GroceryItem.objects.filter(id=item_id))
+    item.delete()
+    return HttpResponseRedirect("../")
 
     
