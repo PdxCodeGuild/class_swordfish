6e0cde81f7213ffcb900e45ada5131c802aba27d
Alden Addor <alden.addor@gmail.com>
2022-10-24 14:21:15 -0600

    Grocery List Complete.

--- code/alden/grocery_list/grocery_list/urls.py
+++ code/alden/grocery_list/grocery_list/urls.py
@@ -6,4 +6,8 @@ app_name = 'grocery_list'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:item_id>/incomplete/', views.incomplete, name='incomplete'),
+    path('<int:item_id>/complete/', views.complete, name='complete'),
+    path('<int:item_id>/delete/', views.delete, name='delete'),
+    path('<add/', views.add, name='add')
 ]
