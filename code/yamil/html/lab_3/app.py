a08980e091ff3862ec343f243600172a6349c0f5
Yamil <yamil.nasser@outlook.com>
2022-10-24 19:47:38 -0400

    Completed HTML Lab 4

--- code/yamil/html/lab_3/app.py
+++ code/yamil/html/lab_3/app.py
@@ -6,30 +6,34 @@ def index():
     posts = [
         {
             'title': "Basketball",
+            'image': "url_for('static',filename='lebron_wade.jpg')",
             'author': "Yamil Nasser",
             'date': "October 1st, 2022",
             'body': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dicta architecto dolorum, numquam enim fugit placeat illum temporibus voluptatem repellat quidem odio inventore sint sed vitae! Ad quia ratione similique accusamus. Fugit quo sed, repudiandae eaque nemo, ipsam eos aperiam ut voluptatem libero aliquid, impedit rem voluptatibus? Voluptatum, cumque molestias."
         },
         {
             'title': "Snowboarding",
+            'image': "url_for('static',filename='snowboarding.jpeg')",
             'author': "Anthony Macias",
             'date': "October 2nd, 2022",
             'body': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dicta architecto dolorum, numquam enim fugit placeat illum temporibus voluptatem repellat quidem odio inventore sint sed vitae! Ad quia ratione similique accusamus. Fugit quo sed, repudiandae eaque nemo, ipsam eos aperiam ut voluptatem libero aliquid, impedit rem voluptatibus? Voluptatum, cumque molestias."
         },
         {
             'title': "Football",
+            'image': "url_for('static',filename='dolphins_tua.jpg')",
             'author': "Edwin Suarez",
             'date': "October 3rd, 2022",
             'body': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dicta architecto dolorum, numquam enim fugit placeat illum temporibus voluptatem repellat quidem odio inventore sint sed vitae! Ad quia ratione similique accusamus. Fugit quo sed, repudiandae eaque nemo, ipsam eos aperiam ut voluptatem libero aliquid, impedit rem voluptatibus? Voluptatum, cumque molestias."
         },
         {
             'title': "Soccer",
+            'image': "url_for('static',filename='messi_v_ronaldo.jpg')",
             'author': "Juan Jurado",
             'date': "October 4th, 2022",
             'body': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dicta architecto dolorum, numquam enim fugit placeat illum temporibus voluptatem repellat quidem odio inventore sint sed vitae! Ad quia ratione similique accusamus. Fugit quo sed, repudiandae eaque nemo, ipsam eos aperiam ut voluptatem libero aliquid, impedit rem voluptatibus? Voluptatum, cumque molestias."
         },
     ]
-    # name = 'bob'
-    # temperature = 67
-    # nums = [1, 2, 3]  
-    return render_template('index.html') #name=name, temperature=temperature, nums=nums)
+    return render_template('index.html', posts=posts)
+
+
+app.run(debug=True)
