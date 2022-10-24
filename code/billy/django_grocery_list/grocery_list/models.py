ba9aa6fad4f0082491a0fb07e15f6b8756236db6
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-10-24 16:35:11 -0700

    Billy Frick - pushed Django lab 1 (incomplete).

---
+++ code/billy/django_grocery_list/grocery_list/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import date
+
+from requests import request
+
+class GroceryItems(models.Model):
+
+    def add_groceries():
+
+        new_item = request.Method
+
+    
