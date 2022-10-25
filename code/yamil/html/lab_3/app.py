a1663f2296cb80452f2630b0762194a8b1ab76b0
YamilNasser <112954622+YamilNasser@users.noreply.github.com>
2022-10-25 18:39:06 -0400

    Completed Lab 3

--- code/yamil/html/lab_3/app.py
+++ code/yamil/html/lab_3/app.py
@@ -6,28 +6,28 @@ def index():
     posts = [
         {
             'title': "Basketball",
-            'image': "url_for('static',filename='lebron_wade.jpg')",
+            'image': 'lebron_wade.jpg',
             'author': "Yamil Nasser",
             'date': "October 1st, 2022",
             'body': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dicta architecto dolorum, numquam enim fugit placeat illum temporibus voluptatem repellat quidem odio inventore sint sed vitae! Ad quia ratione similique accusamus. Fugit quo sed, repudiandae eaque nemo, ipsam eos aperiam ut voluptatem libero aliquid, impedit rem voluptatibus? Voluptatum, cumque molestias."
         },
         {
             'title': "Snowboarding",
-            'image': "url_for('static',filename='snowboarding.jpeg')",
+            'image': 'snowboarding.jpeg',
             'author': "Anthony Macias",
             'date': "October 2nd, 2022",
             'body': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dicta architecto dolorum, numquam enim fugit placeat illum temporibus voluptatem repellat quidem odio inventore sint sed vitae! Ad quia ratione similique accusamus. Fugit quo sed, repudiandae eaque nemo, ipsam eos aperiam ut voluptatem libero aliquid, impedit rem voluptatibus? Voluptatum, cumque molestias."
         },
         {
             'title': "Football",
-            'image': "url_for('static',filename='dolphins_tua.jpg')",
+            'image': 'dolphins_tua.jpg',
             'author': "Edwin Suarez",
             'date': "October 3rd, 2022",
             'body': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dicta architecto dolorum, numquam enim fugit placeat illum temporibus voluptatem repellat quidem odio inventore sint sed vitae! Ad quia ratione similique accusamus. Fugit quo sed, repudiandae eaque nemo, ipsam eos aperiam ut voluptatem libero aliquid, impedit rem voluptatibus? Voluptatum, cumque molestias."
         },
         {
             'title': "Soccer",
-            'image': "url_for('static',filename='messi_v_ronaldo.jpg')",
+            'image': 'messi_v_ronaldo.jpg',
             'author': "Juan Jurado",
             'date': "October 4th, 2022",
             'body': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dicta architecto dolorum, numquam enim fugit placeat illum temporibus voluptatem repellat quidem odio inventore sint sed vitae! Ad quia ratione similique accusamus. Fugit quo sed, repudiandae eaque nemo, ipsam eos aperiam ut voluptatem libero aliquid, impedit rem voluptatibus? Voluptatum, cumque molestias."
