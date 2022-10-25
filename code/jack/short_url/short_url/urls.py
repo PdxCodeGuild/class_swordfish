8f278b3410fcb9e70b6579670287b7b30397a393
jackamoss2 <112007713+jackamoss2@users.noreply.github.com>
2022-10-25 11:43:36 -0700

    removed create view

--- code/jack/short_url/short_url/urls.py
+++ code/jack/short_url/short_url/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'short_url'
 urlpatterns = [
     path('/', views.index, name='index'),
-    path('/create/', views.create, name='create'),
     path('/<str:code>/', views.redirect, name='redirect'),
 ]
--- code/jack/short_url/short_url/views.py
+++ code/jack/short_url/short_url/views.py
@@ -7,27 +7,27 @@ import random, string
 from .models import Url
 
 def index(request):
+    if request.method == 'POST':
+        # all possible characters in code
+        chars = string.ascii_letters + string.digits
+        url = request.POST['url']
+
+        # # no http breaks code - adds if missing
+        # if 'http' not in url: 
+        #     url = 'http://' + url
+
+        code = ''.join([random.choice(chars) for _ in range(6)])
+        
+        # create and save object
+        link = Url(url=url, code=code)
+        link.save()
+
+        return render(request, 'short_url/index.html', {'link':link})
+    
     return render(request, 'short_url/index.html')
-    
-def create(request):
-    # all possible characters in code
-    chars = string.ascii_letters + string.digits
-    url = request.POST['url']
 
-    # # no http breaks code - adds if missing
-    # if 'http' not in url: 
-    #     url = 'http://' + url
-
-    code = ''.join([random.choice(chars) for _ in range(6)])
-    
-    # create and save object
-    link = Url(url=url, code=code)
-    link.save()
-
-    return render(request, 'short_url/index.html', {'link':link})
 
 def redirect(request, code):
     x = get_object_or_404(Url.objects.filter(code=code))
     url = x.url
-
     return HttpResponseRedirect(url)
