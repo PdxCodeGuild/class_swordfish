481a674dfbc90b6f50067690105cc37da66e9755
jackamoss2 <112007713+jackamoss2@users.noreply.github.com>
2022-11-01 10:58:27 -0700

    fixed login redirect bug

--- code/jack/chirp/chirp_project/settings.py
+++ code/jack/chirp/chirp_project/settings.py
@@ -124,3 +124,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# redirects on django user login
+LOGIN_REDIRECT_URL = 'posts:home'
+LOGOUT_REDIRECT_URL = 'posts:home'
