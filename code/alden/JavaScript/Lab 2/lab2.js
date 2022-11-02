881d59f5086841a22df7dd1245dc84e0c971f01f
Alden Addor <alden.addor@gmail.com>
2022-11-02 17:33:33 -0600

    Lab 1 complete, working lab 2

---
+++ code/alden/JavaScript/Lab 2/lab2.js
@@ -0,0 +1,27 @@
+let cards = {
+    'A': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10':10,
+    'J': 10,
+    'Q': 10,
+    'K': 10
+}
+
+let first = prompt('Pick first card: ').toUpperCase()
+let second = prompt('Pick second card: ').toUpperCase()
+let third = prompt('Pick third card: ').toUpperCase()
+
+function value(first, second, third) {
+    let totals = [0]
+    let selections = [first, second, third]
+    for (key in selections)
+        for ()
+
+}
