1064cbe658b81b218bb457ee809647574c0ba19d
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2022-11-03 22:32:38 -0700

    JS Lab 01 in work

--- code/rachel/JavaScript/Lab01.js
+++ code/rachel/JavaScript/Lab01.js
@@ -8,20 +8,39 @@
 // alert (output)
 
 //Blackjack advice//
+
+//adding or removing quotes around keys doesn't seem to affect anything:
+// let card_values = {a: 1, ace: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, j: 10, jack: 10, q: 10, queen: 10, k: 10, king: 10} 
+
 let card_values = {a: 1, Ace: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, j: 10, jack: 10, q: 10, queen: 10, k: 10, king: 10}
 
-let first_card = prompt("What's your first card? ")
-let second_card = prompt("What's your second card? ")
-let third_card = prompt("What's your third card? ")
+var first_card = prompt("What's your first card? ")
+var second_card = prompt("What's your second card? ")
+var third_card = prompt("What's your third card? ")
 
-let cards_total = first_card + second_card + third_card
+//after asking for the 3 card values, didn't return anything:
+let value_1 = card_values.first_card
+let value_2  = card_values.second_card
+let value_3 = card_values.third_card
 
+//after asking for the 3 card values, didn't return anything:
+// value_1 = parseInt(value_1)
+// value_2 = parseInt(value_2)
+// value_3 = parseInt(value_3)
+//after asking for the 3 card values, didn't return anything:
+// let cards_total = card_values.first_card + card_values.second_card + card_values.third_card 
+
+//neither seems to make a distance - can't move past entering numbers
+let cards_total = value_1 + parseInt + parseInt
+//let cards_total = parseInt(value_1) + parseInt(value_2) + parseInt(value_3)
+
+//adding ${} around card_totals for each alert: program wouldn't even run
 if (cards_total <= 16) {
-    alert(cards_total + "hit")
+    alert(cards_total + " hit")
 } else if (cards_total <= 20) {
-    alert(cards_total + "stay")
+    alert(cards_total + " stay")
 } else if (cards_total == 21) {
-    alert(cards_total + "blackjack!")
+    alert(cards_total + " blackjack!")
 } else if (cards_total > 21) { 
-    alert(cards_total + "Busted!")
+    alert(cards_total + " Busted!")
 }
