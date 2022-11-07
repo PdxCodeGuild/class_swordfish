fb83549dd02ad93d85f4ae59b5ad822561be98a8
Alden Addor <alden.addor@gmail.com>
2022-11-07 16:42:19 -0700

    Lab 2B and 3B completed.

--- code/alden/JavaScript/Lab 3B/lab3b.js
+++ code/alden/JavaScript/Lab 3B/lab3b.js
@@ -13,24 +13,13 @@ let inUnit = document.getElementById('in-unit')
 let out_value = document.getElementById('out_value')
 
 
-
-
-
-
 convert.addEventListener('click', function(){
-    console.log('inUnit', inUnit)
     let input_value = meters[inUnit.value]
     let in_conversion = parseFloat(distance.value) * input_value
     for (key in meters){
         if (out_value.value == key){
-            // console.log(key)
             let conversion = in_conversion / meters[key]
-            // console.log(conversion)
             result.innerText = conversion
         }
     }
-    // console.log(in_unit.value)
-    console.log('input_value', input_value)
-    
-    // console.log(in_conversion)
 })
