2d062fbd5a93e1bd3fa8ddca1551b6f6ce140f20
ktabusa <ktabusa@gmail.com>
2022-11-09 15:19:01 -0800

    updated for saving local data information. Needs to load local data

--- code/Group1_JS/app.js
+++ code/Group1_JS/app.js
@@ -3,7 +3,7 @@ let addItemBtn = document.getElementById("add_item")
 let todoDiv = document.getElementById("todo-div")
 let completeDiv = document.getElementById("complete-div")
 
-// pulling from browser local storage
+// pulling from browser local storage, the ? is the ternary operator to say if data exists use, or the : is to set it as an empty array
 let data_complete = localStorage.getItem('data_complete') ? JSON.parse(localStorage.getItem('data_complete')) : []
 let data_incomplete = localStorage.getItem('data_incomplete') ? JSON.parse(localStorage.getItem('data_incomplete')) : []
 
@@ -23,6 +23,7 @@ function addForReal() {
         newTodo.remove()
         data_complete.push(input_string) // add item to data storage variable
         localStorage.setItem("data_complete", JSON.stringify(data_complete)) // save to local storage
+        data_incomplete = removeData(data_incomplete, input_string, 'data_incomplete') 
         completeDiv.appendChild(newTodo)
         newItemComplete.remove()
     })
@@ -30,14 +31,29 @@ function addForReal() {
     let removeItem = document.createElement("button")
     removeItem.innerText = "x"
     removeItem.addEventListener('click', function() {
-        newTodo.remove()
         removeItem.remove()
         newItemComplete.remove()
+        category = newTodo.parentElement.id
+        console.log(newTodo)
+        if (category == 'complete-div') {
+            removeData(data_complete, input_string, 'data_complete')
+        }
+        else if (category == 'todo-div') {
+            removeData(data_incomplete, input_string, 'data_incomplete')
+        }
+        newTodo.remove()
     })
 
     newTodo.appendChild(newItemComplete)
     todoDiv.appendChild(newTodo)
     newTodo.appendChild(removeItem)
+}
+
+function removeData(array, data, name) {
+    varIndex = array.indexOf(data)
+    array.splice(varIndex, 1)
+    localStorage.setItem(name, JSON.stringify(array)) // this line added to update local storage every run
+    return array
 }
 
 addItemBtn.addEventListener('click', addForReal)
