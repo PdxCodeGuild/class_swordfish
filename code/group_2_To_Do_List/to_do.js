f8ea7b3d8771b81003ccad3f4c41d997c65bbec7
VanguardAWS <ethanturner93@hotmail.com>
2022-11-09 15:41:06 -0600

    pushed changes

--- code/group_2_To_Do_List/to_do.js
+++ code/group_2_To_Do_List/to_do.js
@@ -6,7 +6,28 @@ let task = document.getElementById('task')
 
 addTask.addEventListener('click', function() {
     let newTask = document.createElement('li')
-    newTask.innerText = task
+    newTask.innerText = task.value
     console.log(newTask)
     console.log(task)
+    taskList.append(newTask)
+    
+    
+    let newTaskStatus = document.createElement("button")
+    newTaskStatus.innerText = "delete"
+    newTaskStatus.addEventListener('click', function() {
+        newTask.previousSibling.remove()
+        newTask.remove()
+        newTaskStatus.remove()
+    })
+    
+    taskList.append(newTaskStatus)
+
+    let completedStatus = document.createElement("input")
+    completedStatus.type = "checkbox"
+    
+    completedStatus.addEventListener('click', function() {
+        completedStatus.value = True
+    })
+    
+    taskList.append(completedStatus)
 })
