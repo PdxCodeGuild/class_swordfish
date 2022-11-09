38b9db131a1a1d5830279a1ffc7125b3320d4eef
Chris Kee <ckeego@Chriss-MacBook-Pro.local>
2022-11-09 15:17:13 -0500

    Alpha Group Lab

---
+++ code/alpha_group/app.js
@@ -0,0 +1,38 @@
+let adds = document.getElementById("add_list")
+let addButton = document.getElementById("add")
+let textDiv = document.getElementById("text-div")
+let todoUL = document.getElementById("element")
+
+addButton.addEventListener('click', function() {
+    let listElement = document.createElement("li")
+    listElement.innerText = adds.value
+    todoUL.append(listElement)
+    // for (let i=0; i < todoList.length; i++) {
+    //     todoList[i].innerText = adds.value
+    //     }
+    
+
+
+    let newTextRemove = document.createElement("button")
+    newTextRemove.innerText = "x"
+    newTextRemove.addEventListener('click', function() {
+        // listElement.previousSibling.remove()
+        listElement.remove()
+        newTextRemove.remove()
+    })
+
+    listElement.appendChild(newTextRemove)
+    console.log(listElement)
+    console.log(listElement.innerText)
+    let completeButton = document.createElement("button")
+    completeButton.innerText = "C"
+    completeButton.addEventListener('click', function() {
+        let strike = document.createElement("s")
+        strike.innerText = listElement.value
+        console.log(strike.innerText)
+        listElement.strike()
+    })
+    
+    listElement.appendChild(completeButton)
+    
+})
