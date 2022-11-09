dba2712d64e0e5ed20849f04a4cb05f06335036d
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-11-08 16:46:13 -0800

    pushed changes to lab 1, part b.

--- code/billy/javascript/lab_01-03_part_b/blackjack_advice_part_b.js
+++ code/billy/javascript/lab_01-03_part_b/blackjack_advice_part_b.js
@@ -1,4 +1,4 @@
-let face_cards = {
+const face_cards = {
     king: 10,
     queen: 10,
     jack: 10,
@@ -14,5 +14,23 @@ let face_cards = {
     ace: 1,
 }
 let submit = document.getElementById('submit')
-let card_score = document.getElementById('card_score')
 
+submit.addEventListener('click', function() {
+    let card_one = document.getElementById('card_one').value.toUpperCase()
+    let card_two = document.getElementById('card_two').value.toUpperCase()
+    let card_three = document.getElementById('card_three').value.toUpperCase()
+    let card_selection = [card_one, card_two, card_three]
+
+    let card_score = [0]
+    for (let card of card_selection) {     
+        for (let key of Object.getOwnPropertyNames(face_cards)) { // cycles through all options to try to match
+            if (key == card) {
+                for (let i in total) { // increments each total score scenario, aka number in list
+                    card_score[i] += face_cards[key]
+                }
+            }
+        }
+        
+    }
+    console.log(card_score)
+})
