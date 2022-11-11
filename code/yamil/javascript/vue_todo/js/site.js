aab26ad4f8ab4f76d067d97a37ace60baa80cb63
YamilNasser <112954622+YamilNasser@users.noreply.github.com>
2022-11-10 19:31:33 -0500

    Updated Vue lab

---
+++ code/yamil/javascript/vue_todo/js/site.js
@@ -0,0 +1,10 @@
+var todo_list = new Vue({
+    el: '#todo_list',
+    data: {
+      todos: [
+        { text: 'Test' },
+        { text: 'Test1' },
+        { text: 'Test2' }
+      ]
+    }
+  })
