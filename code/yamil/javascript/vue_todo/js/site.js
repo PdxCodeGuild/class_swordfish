b717d3018afd90de138ca17f7fcf7881a657bbfa
YamilNasser <112954622+YamilNasser@users.noreply.github.com>
2022-11-14 19:31:54 -0500

    Updated Vue Lab

--- code/yamil/javascript/vue_todo/js/site.js
+++ code/yamil/javascript/vue_todo/js/site.js
@@ -1,10 +1,21 @@
-var todo_list = new Vue({
-    el: '#todo_list',
+// python -m http.server
+
+const todoListVue = new Vue({
+    el: '#todo-list',
     data: {
-      todos: [
-        { text: 'Test' },
-        { text: 'Test1' },
-        { text: 'Test2' }
-      ]
-    }
+      newTask : "",
+      completedTask: "",
+      todos: [],
+      completed: []
+    },
+    methods: {
+      addTodo: function() {
+        this.todos.push({text: this.newTask})
+      },
+      completeTodo: function() {
+          this.completed.push({text: this.})
+        },
+      }
+    
   })
+  
