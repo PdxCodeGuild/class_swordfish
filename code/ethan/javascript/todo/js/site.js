16fe00fedd19c7543d5ea2577143d5320125aae5
VanguardAWS <ethanturner93@hotmail.com>
2022-11-15 09:55:16 -0600

    Ethan-Javascript Lab04 complete

--- code/ethan/javascript/todo/js/site.js
+++ code/ethan/javascript/todo/js/site.js
@@ -1,16 +1,23 @@
 var app4 = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue',
-      todos: [
-        { text: 'Learn JavaScript' },
-        { text: 'Learn Vue' },
-        { text: 'Build something awesome' }
-      ]
+      message: 'Todo text',
+      incompleteTodos: [],
+      completeTodos: [],
     },
     methods: {
         addTodo: function() {
-            todos.push(this.message)
+          this.incompleteTodos.push(this.message)
+        },
+        deleteTodo: function(index) {
+          this.incompleteTodos.splice(index, 1)
+        },
+        deleteCompleteTodo: function(index) {
+          this.completeTodos.splice(index, 1)
+        },
+        completeTodo: function(index) {
+          this.completeTodos.push(this.message)
+          this.incompleteTodos.splice(index, 1)
         }
     }
   })
