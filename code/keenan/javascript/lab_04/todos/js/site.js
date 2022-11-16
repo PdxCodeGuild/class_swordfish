3abc97f257f4721d1e33e05d7c20876e7f1cb01a
ktabusa <ktabusa@gmail.com>
2022-11-16 14:26:27 -0800

    updated the incomplete complete todos computed properties to function and display properly

--- code/keenan/javascript/lab_04/todos/js/site.js
+++ code/keenan/javascript/lab_04/todos/js/site.js
@@ -1,12 +1,38 @@
-new Vue({
-	el: "#app",
-	data:{
-        message: 'Hello world!'
+const vm = new Vue({
+	el: '#app',
+	data: {
+        // created placeholder data
+        todos: [
+            { id: 1, text: "walk the dog", completed: false, createdDate: new Date(), },
+		    { id: 2, text: "learn vue", completed: true, createdDate: new Date(), },
+		    { id: 3, text: "drink water", completed: true, createdDate: new Date(), }
+        ]
 	},
-	methods{
+	methods:{
+        addTodo: function() {
+            let newItem = {
 
+            }
+        }
 	},
-	computed{
-
+	computed:{
+        incompleteTodos: function() {
+            const temp = [] // this variable only exists in this function
+            for(let todo of this.todos) { // this loops through each of the todos
+                if(todo.completed===false){ 
+                    temp.push(todo) // adds the todo to the 
+                }
+            }
+            return temp
+        },
+        // this can also be completed with Array.filter() and works the same as above
+        completeTodos: function() {
+            return this.todos.filter(function(item) {
+                return item.completed===true
+            })
+        
+            // return this.todos.filter(item => item.completed === true);
+            //  Option 3: his was another example from the documentation that works the same             
+        }
 	}
 })
