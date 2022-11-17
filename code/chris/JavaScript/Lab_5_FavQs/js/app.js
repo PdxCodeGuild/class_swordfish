f8c6c27cfcff9635aea0c04b719d9379c3721024
Chris Kee <ckeego@Chriss-MacBook-Pro.local>
2022-11-16 19:26:31 -0500

    JS Lab 5

--- code/chris/JavaScript/Lab_5_FavQs/js/app.js
+++ code/chris/JavaScript/Lab_5_FavQs/js/app.js
@@ -1,25 +1,73 @@
+Vue.component('quote-block', {
+    data: function() {
+        return {
+
+        }
+    },
+    props: ['quote'],
+    template: 
+    `<div>
+        <ul>
+            <li>{{quote.body}} -- {{quote.author}}</li>
+        </ul>
+    </div>`
+})
+
 const favQ = new Vue ({
     el: "#favQapp",
     data: {
-        randomQuotes: []
+        randomQuotes: [],
+        search: "",
+        keywordQuoteslist: []
     },
 
     methods: {
         getQuotes: function() {
             this.randomQuotes = []
-            axios({
-                method: "GET",
-                url: "https://favqs.com/api/qotd",
-                headers: {
-                    'Authorization': `Token token="855df50978dc9afd6bf86579913c9f8b"`
-                }
-            }).then(function() {}
-            
-            })
-        }
+            this.keywordQuoteslist = []
+            // JS for loop
+            for (let i=0; i<5; ++i) {
+                axios({
+                    method: "GET",
+                    url: "https://favqs.com/api/qotd",
+                    headers: {
+                        'Authorization': `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then((response) => { //arrow Function!!!
+                    // this.randomQuotes = response.data
+                    let singleQuote = response.data.quote
+                    this.randomQuotes.push(singleQuote)
+                })
+            }
+        }, 
+        keywordQuotes: function() {
+            this.randomQuotes = []
+            this.keywordQuoteslist = []
+                axios({
+                    method: "GET",
+                    url: "https://favqs.com/api/quotes",
+                    params: {
+                        filter: this.search,
+                        type: 'keyword'
+                    },
+                    headers: {
+                        'Authorization': `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then((response) => { //arrow Function!!!
+                    console.log(response) // got 200 message
+                    this.keywordQuoteslist = response.data.quotes
+                    this.keyword = ""
+                })
+        },
+        nextPage: function() {
+     
+        },
     },
 
     computed: {
-
+    
     }, 
+    created: function() {
+        this.getQuotes()
+    }
 })
