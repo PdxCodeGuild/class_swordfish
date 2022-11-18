8fc1702451b57cc87c9832a212a97df3db09fa18
Chris Kee <ckeego@Chriss-MacBook-Pro.local>
2022-11-17 19:24:23 -0500

    JS Lab 5

--- code/chris/JavaScript/Lab_5_FavQs/js/app.js
+++ code/chris/JavaScript/Lab_5_FavQs/js/app.js
@@ -16,15 +16,25 @@ Vue.component('quote-block', {
 const favQ = new Vue ({
     el: "#favQapp",
     data: {
-        randomQuotes: [],
+        theRandomQuotes: [],
+        keywordQuoteslist: [],
+        tagQuoteslist: [],
+        authorQuoteslist: [],
+        page: 1,
+        inputText: "",
         search: "",
-        keywordQuoteslist: []
+        tags: "",
+        author: "",
+        currentType: "",
+        currentSearch: ""
     },
 
     methods: {
         getQuotes: function() {
-            this.randomQuotes = []
+            this.theRandomQuotes = []
             this.keywordQuoteslist = []
+            this.authorQuoteslist = []
+            this.tagQuoteslist = []
             // JS for loop
             for (let i=0; i<5; ++i) {
                 axios({
@@ -36,38 +46,106 @@ const favQ = new Vue ({
                 }).then((response) => { //arrow Function!!!
                     // this.randomQuotes = response.data
                     let singleQuote = response.data.quote
-                    this.randomQuotes.push(singleQuote)
+                    this.theRandomQuotes.push(singleQuote)
                 })
             }
         }, 
         keywordQuotes: function() {
-            this.randomQuotes = []
+            this.search = this.inputText
+            this.inputText = ""
+            this.theRandomQuotes = []
             this.keywordQuoteslist = []
+            this.page = 1
                 axios({
                     method: "GET",
                     url: "https://favqs.com/api/quotes",
                     params: {
                         filter: this.search,
-                        type: 'keyword'
                     },
                     headers: {
                         'Authorization': `Token token="855df50978dc9afd6bf86579913c9f8b"`
                     }
                 }).then((response) => { //arrow Function!!!
-                    console.log(response) // got 200 message
                     this.keywordQuoteslist = response.data.quotes
-                    this.keyword = ""
                 })
         },
-        nextPage: function() {
-     
+        authorQuotes: function() {
+            this.currentSearch = this.inputText
+            this.currentType = "author"
+            this.inputText = ""
+            this.theRandomQuotes = []
+            this.authorQuoteslist = []
+            this.page = 1
+                axios({
+                    method: "GET",
+                    url: "https://favqs.com/api/quotes",
+                    params: {
+                        type: this.currentType,
+                        filter: this.currentSearch
+                    },
+                    headers: {
+                        'Authorization': `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then((response) => { //arrow Function!!!
+                    this.authorQuoteslist = response.data.quotes
+                    console.log("author data", response.data.quotes)
+                })
         },
+        taggingQuotes: function() {
+            this.currentSearch = this.inputText
+            this.currentType = "tag"
+            this.inputText = ""
+            this.theRandomQuotes = []
+            this.tagQuoteslist = []
+            this.page = 1
+                axios({
+                    method: "GET",
+                    url: "https://favqs.com/api/quotes",
+                    params: {
+                        type: this.currentType,
+                        filter: this.currentSearch
+                    },
+                    headers: {
+                        'Authorization': `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then((response) => { //arrow Function!!!
+                    this.tagQuoteslist = response.data.quotes
+                })
+        },
+        nextPage: function() { 
+            this.page++
+            this.theRandomQuotes = []
+            this.keywordQuoteslist = []
+            this.tagQuoteslist = []
+            this.authorQuoteslist = []
+                axios({
+                    method: "GET",
+                    url: "https://favqs.com/api/quotes",
+                    params: {
+                        filter: this.currentSearch,
+                        type: this.currentType,
+                        page: this.page
+                    },
+                    headers: {
+                        'Authorization': `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                    }
+                }).then((response) => { //arrow Function!!!
+                    if(this.currentType === 'author') {
+                        this.authorQuoteslist = response.data.quotes
+                    } else if(this.currentType === 'tag') {
+                        this.tagQuoteslist = response.data.quotes
+                    } else {
+                        this.keywordQuoteslist = response.data.quotes
+                    }
+                    this.page = response.data.page
+                })
+        },
+        
     },
-
     computed: {
     
     }, 
     created: function() {
         this.getQuotes()
-    }
+    },
 })
