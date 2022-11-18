6d1f9c7ec127d6ddff8d1c80c574538cc4071950
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-11-18 15:52:30 -0800

    billy frick- pushed working code for lab 5, pagination still needs to be implemented.

--- code/billy/javascript/lab_05/js/app.js
+++ code/billy/javascript/lab_05/js/app.js
@@ -1,7 +1,16 @@
-new Vue({
+const vm = new Vue({
   el: '#app',
   data: {
-    quotes: []
+    inputType: '', //We want this to allow the user to choose author, keyword or tag.
+    inputTerm: '', //What the user types in.
+    
+    currentType: '',
+    currentTerm: '', //What the user last used.
+
+    currentPage: 1,
+
+    newParamter: '',
+    responseData: {},
   },
   methods: {
     getQuotes: function() {
@@ -9,23 +18,48 @@ new Vue({
         method: 'GET',
         url: 'https://favqs.com/api/quotes',
         headers: {
-          'Authorization': 'Token token="9eb8fe5d65bc94d9d25212fa234b6455"'
+          'Authorization': 'Token token="9eb8fe5d65bc94d9d25212fa234b6455", Content-Type: application/json'
         },
-      }).then((response) => {
-        this.quotes = response.data
+
+      }).then((response) => { 
+        console.log('response', response)
+        this.responseData = response.data
+
       })
+    },
+    searchQuotes: function(){
+      this.currentTerm = this.inputTerm
+      this.currentType = this.inputType
+      axios({
+        method: 'GET',
+        url: 'https://favqs.com/api/quotes/?filter=""&type=""',
+        headers: {
+          'Authorization': 'Token token="9eb8fe5d65bc94d9d25212fa234b6455", Content-Type: application/json'
+        },
+        params: {
+          filter: this.currentTerm,
+          type: this.currentType,
+        }
+
+      }).then((response) => { 
+        console.log('response', response)
+        this.responseData = response.data
+      })
+    },
+    nextPage: function() {
+      console.log('trying to get the next page')
+     
     }
   },
-  computed: {
 
-  },
-  created: function(){
+  computed: {
 
   },
   beforeMount: function() {
     this.getQuotes()
-  },
-  mounted: function(){
+  }
 
-  },
+  
 })
+
+//this is the path to 'this.responseData.last_page'
