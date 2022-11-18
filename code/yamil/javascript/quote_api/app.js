a62167b8ecc980dae062db4deb548f3f9a1ef759
YamilNasser <112954622+YamilNasser@users.noreply.github.com>
2022-11-17 19:31:03 -0500

    updated quote api lab

---
+++ code/yamil/javascript/quote_api/app.js
@@ -0,0 +1,95 @@
+// python -m http.server
+
+const quotesApiVue = new Vue({
+  el: '#app',
+  data: {
+    newSearch: "",
+    quotes: [],
+    searchParameter: ""
+  },
+  computed: {
+  
+  },
+  methods: {
+    // searchParameterFunction: function() {
+    //   if (this.searchParameter === "keyword") {
+    //     this.keywordSearch()
+    //   }
+    //   else if (this.searchParameter === "author") {
+    //     this.authorSearch()
+    //   }
+    //   else if (this.searchParameter === "tag") {
+    //     this.tagSearch()
+    //   }
+    // },
+    nextPage: function() {
+      this.page++
+    },
+    previousPage: function() {
+      this.page = this.page - 1
+    },
+// ----------------------------------------------------------------------------------------------------
+    authorSearch: function() {
+      console.log(this.quotes.page, "quotes.page")
+      console.log(this.quotes.last_page, "last page")
+      author = `?filter=${this.newSearch}&type=author`,
+      axios({
+        method: "GET",
+        url: `https://favqs.com/api/quotes/`+`${author}`,
+        headers: {
+        'Authorization': `Token token="3b3e469abc960df09cbcc8ab225ea0f0"`
+        }
+      }).then((response) => {
+        this.quotes = response.data
+        this.page = response.data.page
+      })
+      this.newSearch = ""
+    },
+
+    tagSearch: function() {
+      tag = `?filter=${this.newSearch}&type=tag`,
+      axios({
+        method: "GET",
+        url: `https://favqs.com/api/quotes/`+`${tag}`,
+        headers: {
+        'Authorization': `Token token="3b3e469abc960df09cbcc8ab225ea0f0"`
+        }
+      }).then((response) => {
+        this.quotes = response.data
+        this.page = response.data.page
+      })
+      this.newSearch = ""
+    },
+
+    keywordSearch: function() {
+      keyword = `?filter=${this.newSearch}`,
+      axios({
+        method: "GET",
+        url: `https://favqs.com/api/quotes/`+`${keyword}`,
+        headers: {
+        'Authorization': `Token token="3b3e469abc960df09cbcc8ab225ea0f0"`
+        }
+      }).then((response) => {
+        this.quotes = response.data
+        this.page = response.data.page
+      })
+      this.newSearch = ""
+    },
+
+    getQuotes: function() {
+      axios({
+        method: "GET",
+        url: "https://favqs.com/api/quotes/",
+        headers: {
+        'Authorization': `Token token="3b3e469abc960df09cbcc8ab225ea0f0"`
+        }
+      }).then((response) => {
+        this.quotes = response.data
+        this.page = response.data.page
+      })
+    }
+  },
+  beforeMount: function() {
+    this.getQuotes()
+  }
+})
