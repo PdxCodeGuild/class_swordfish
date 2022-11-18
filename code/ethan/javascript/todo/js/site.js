c3e24a7842e2978988c331eca582d45db5d56d23
VanguardAWS <ethanturner93@hotmail.com>
2022-11-18 12:39:20 -0600

    Lab04 in progress

--- code/ethan/javascript/todo/js/site.js
+++ code/ethan/javascript/todo/js/site.js
@@ -2,8 +2,11 @@ var app4 = new Vue({
     el: '#app',
     data: {
       message: 'Todo text',
-      incompleteTodos: [{text: 'walk the dog', completed: false}],
-      completeTodos: [],
+      todos: [
+        {text: 'walk the dog', completed: false},
+        {text: 'brush teeth', completed: false},
+        {text: 'do homework', completed: true},
+      ],
     },
     methods: {
         addTodo: function() {
@@ -30,6 +33,27 @@ var app4 = new Vue({
         incompleteTodo: function(index) {
           this.incompleteTodos.push(index)
           this.completeTodos.splice(index, 1)
-        }
+        },
+    
+    },
+    computed: {
+      incompleteTodos: function() {
+        return this.todos.filter(
+          function(
+            todo
+          ){
+            return todo.completed === false
+          }
+        )
+      },
+      completeTodos: function() {
+        return this.todos.filter(
+          function(
+            todo
+          ){
+            return todo.completed === true
+          }
+        )
+      }
     }
   })
