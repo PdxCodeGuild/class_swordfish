fd4bef7a9547307d583785037bf2fc3244d31c94
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-11-18 18:48:23 -0500

    Lab03 Part A in progress

--- code/sarde/javascript/lab03/lab03.js
+++ code/sarde/javascript/lab03/lab03.js
@@ -36,53 +36,53 @@ Version2
 
 //create a function that allows the user to create a contact
 //add the created contact contacts
-// function createContact(theKeys, contacts) {
-//     //create a record
-//     // ask the user for name, favorite fruit, favorite color
-//     let userName = prompt('Enter your name')
-//     let userfavoriteFruit = prompt('Enter your favortie fruit')
-//     let userfavoriteColor = prompt('Enter your favorite color')
-//     // add what the user inputs to contacts
-//     let newContact = {
-//         name: userName,
-//         favoriteFruit: userfavoriteFruit,
-//         favoriteColor: userfavoriteColor
-//     }
-//     //use some method to add the newContact to contacts
-//     contacts.push(newContact)
-//     console.log(newContact)
-//     // alert(newContact)
-//     console.log(contacts)
+function createContact(theKeys, contacts) {
+    //create a record
+    // ask the user for name, favorite fruit, favorite color
+    let userName = prompt('Enter your name')
+    let userfavoriteFruit = prompt('Enter your favortie fruit')
+    let userfavoriteColor = prompt('Enter your favorite color')
+    // add what the user inputs to contacts
+    let newContact = {
+        name: userName,
+        favoriteFruit: userfavoriteFruit,
+        favoriteColor: userfavoriteColor
+    }
+    //use some method to add the newContact to contacts
+    contacts.push(newContact)
+    console.log(newContact)
+    // alert(newContact)
+    console.log(contacts)
 
-//     return contacts
+    return contacts
 
-// }
-// createContact(theKeys, contacts)
-// // console.log(contacts)
+}
+createContact(theKeys, contacts)
+// console.log(contacts)
 
-// //create a function where the user enters a contacts name, and then that name is logged for the user
-// function retrieveContact(contacts) {
-//     //ask the user for the name to search for
-//     let nameToSearchFor = prompt('Enter contact\'s name')
-//     //iterate over contacts
-//     contacts.forEach(function (contact) {
-//         // console.log(contact)
-//         let name = contact['name']
-//         //if the name entered by the user is in contacts
-//         //print that contact 
-//         if (name == nameToSearchFor) {
-//             // console.log(name)
-//             console.log(contact)
-//         }
-//         return contact
-//     });
-// }
-// retrieveContact(contacts)
+//create a function where the user enters a contacts name, and then that name is logged for the user
+function retrieveContact(contacts) {
+    //ask the user for the name to search for
+    let nameToSearchFor = prompt('Enter the contact\'s name you want to retrieve')
+    //iterate over contacts
+    contacts.forEach(function (contact) {
+        // console.log(contact)
+        let name = contact['name']
+        //if the name entered by the user is in contacts
+        //print that contact 
+        if (name == nameToSearchFor) {
+            // console.log(name)
+            console.log(contact)
+        }
+        return contact
+    });
+}
+retrieveContact(contacts)
 
 //create a function where the user can update a contact
 function updateContact(contacts) {
     //ask the user for the contacts name
-    let nameToUpdate = prompt('Enter contact\'s name')
+    let nameToUpdate = prompt('Enter the contact\'s name you want to update')
 
     //iterate over the length of the contacts list
     for (let i = 0; i < contacts.length; i++) {
@@ -98,3 +98,42 @@ function updateContact(contacts) {
     return contacts
 }
 updateContact(contacts)
+
+function deleteContact(contacts) {
+    //ask the user for the contact's name
+    let nameToDelete = prompt('Enter the contact\'s name you want to delete')
+    // iterate over the contacts
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i]['name'] == nameToDelete) {
+            delete contacts[i]
+        }
+    }
+    return contacts
+}
+deleteContact(contacts)
+
+//REPL Loop
+let stop = false
+while (!stop) {
+    let response = prompt('Enter a choice(q, c, r, u, d): ')
+    if (response == 'q') {
+        console.log('Person wants to quit')
+        let stop = true
+    } else if (response == 'c') {
+        console.log('Person wants to create')
+        let contacts = createContact(theKeys, contacts)
+        //console.log(contacts)
+    } else if (response == 'r') {
+        console.log('Person wants to retrieve')
+        let contact = retrieveContact(contacts)
+        console.log(contact)
+    } else if (response == 'u') {
+        console.log('Person wants to update')
+        let contacts = updateContact(contacts)
+        //console.log(contacts)
+    } else if (response == 'd') {
+        console.log('Person wants to delete')
+        let contacts = deleteContact(contacts)
+    }
+}
+    //console.log(contacts)
