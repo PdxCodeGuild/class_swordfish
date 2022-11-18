2f1fad20ce5a7e98824263a5e12ab66182e122d9
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-11-17 16:30:00 -0800

    billy frick - pushed lab 5, had troubles with my installs working properly. I also had to troubleshoot my laptop for it constantly overheating today. Will push more of lab 5 later tonight.

--- code/billy/javascript/lab_05/js/app.js
+++ code/billy/javascript/lab_05/js/app.js
@@ -1,20 +1,31 @@
-import Vue from 'vue';
-
-window.axios = require('axios');
-
 new Vue({
   el: '#app',
-
-  components: {
-
-  },
-
-  mounted: function (){
-    axios.get('https://favqs.com/api/quotes')
-      .then(response => console.log(response))
-  },
-
   data: {
+    quotes: []
+  },
+  methods: {
+    getQuotes: function() {
+      axios({
+        method: 'GET',
+        url: 'https://favqs.com/api/quotes',
+        headers: {
+          'Authorization': 'Token token="9eb8fe5d65bc94d9d25212fa234b6455"'
+        },
+      }).then((response) => {
+        this.quotes = response.data
+      })
+    }
+  },
+  computed: {
 
-  }
+  },
+  created: function(){
+
+  },
+  beforeMount: function() {
+    this.getQuotes()
+  },
+  mounted: function(){
+
+  },
 })
