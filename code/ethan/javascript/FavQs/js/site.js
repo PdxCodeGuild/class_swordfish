20785815dd3fcf073dd616cc4e6a7763f6c7f00c
VanguardAWS <ethanturner93@hotmail.com>
2022-11-17 18:31:31 -0600

    Ethan Lab05 in progress

--- code/ethan/javascript/FavQs/js/site.js
+++ code/ethan/javascript/FavQs/js/site.js
@@ -1,6 +1,45 @@
+Vue.component('quotes', {
+  data: function() {
+      return {
+      }
+  },
+  props: ['quote'],
+  template: 
+  `<div>
+      <ul>
+          <li>{{quote.body}}<br>
+           - {{quote.author}}</li>
+      </ul>
+  </div>`
+})
+
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
+  el: '#app',
+  data: {
+    search: "",
+    quotes: {},
+    parameters:{},
+
+  },
+  methods: {
+    getQuotes: function() {
+      axios({
+          method: "GET",
+          url: "https://favqs.com/api/quotes/",
+          headers: {
+              'Authorization': 'Token token="9daa8830e1b3e3a7b44eda726857db04"'
+          },
+          params: this.parameters
+      }).then((response) => {
+          this.quotes = response.data
+          console.log(this.data)
+      }) 
+    }  
+  },
+  computed: {
+
+  },
+  beforeMount: function () {
+      this.getQuotes()
+  }
+})
--- code/ethan/javascript/todo/js/site.js
+++ code/ethan/javascript/todo/js/site.js
@@ -2,13 +2,14 @@ var app4 = new Vue({
     el: '#app',
     data: {
       message: 'Todo text',
-      incompleteTodos: [{text: 'walk the dog', completed:false}],
+      incompleteTodos: [{text: 'walk the dog', completed: false}],
       completeTodos: [],
     },
     methods: {
         addTodo: function() {
           let newTodo = {
-            text: this.message}
+            text: this.message,
+            completed: false}
           this.incompleteTodos.push(newTodo),
           this.message = ''
         },
@@ -19,7 +20,11 @@ var app4 = new Vue({
           this.completeTodos.splice(index, 1)
         },
         completeTodo: function(index) {
-          this.completeTodos.push(index)
+          let completeTodo = {
+            text: index.text,
+            completed: true
+          }
+          this.completeTodos.push(completeTodo)
           this.incompleteTodos.splice(index, 1)
         },
         incompleteTodo: function(index) {
