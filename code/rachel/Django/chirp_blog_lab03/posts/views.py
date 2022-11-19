0d037beef85db27b5aac3db88bbfffaaebd2c90f
rcleav <rcleav@rcleavs-MacBook-Pro.local>
2022-11-18 16:33:30 -0800

    DJ Lab 3 in work

--- code/rachel/Django/chirp_blog_lab03/posts/views.py
+++ code/rachel/Django/chirp_blog_lab03/posts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'content']
+
+class BlogEditView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'content']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts:home')
---
+++ code/rachel/Django/chirp_blog_lab03/users/views.py
@@ -0,0 +1,10 @@
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
+
+
