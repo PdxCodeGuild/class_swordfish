effa94d55f9b267f2472ec78ad72565dcd7743ea
anthony <anthonyt.wallace@icloud.com>
2022-11-22 13:51:11 -0800

    Added Viewset example

--- code/anthony/drf_todos/todos/urls.py
+++ code/anthony/drf_todos/todos/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from .views import ListTodo, DetailTodo, index
+from rest_framework import routers
+# from .views import ListTodo, DetailTodo, index 
+from .views import TodoViewSet
 
-urlpatterns = [
-    # http://localhost:8000/todos
-    path("", ListTodo.as_view()),
-    # http://localhost:8000/todos/2
-    path("<int:pk>/", DetailTodo.as_view()),
-    path("home", index, name="index")
-]
+
+router = routers.DefaultRouter()
+
+# http://localhost:8000/todos
+router.register('', TodoViewSet)
+
+urlpatterns = router.urls
+
+
+
+
+# urlpatterns = [
+#     # http://localhost:8000/todos
+#     path("", ListTodo.as_view()),
+#     # http://localhost:8000/todos/2
+#     path("<int:pk>/", DetailTodo.as_view()),
+#     path("home", index, name="index")
+# ]
--- code/anthony/drf_todos/todos/views.py
+++ code/anthony/drf_todos/todos/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Todo
 from .serializers import TodoSerializer
 
-class ListTodo(generics.ListCreateAPIView):
-    # queryset => specifies which data to return
+
+
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
-    # serializer_class => specifies which serializer to use
     serializer_class = TodoSerializer
 
 
-class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+
+# Using generics
+# class ListTodo(generics.ListCreateAPIView):
+#     # queryset => specifies which data to return
+#     queryset = Todo.objects.all()
+#     # serializer_class => specifies which serializer to use
+#     serializer_class = TodoSerializer
+
+
+# class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
 
 
 
