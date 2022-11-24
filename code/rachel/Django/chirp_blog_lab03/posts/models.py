3e233997cd4a659632c16d9109b821edb827fd2d
junedeor <112974139+junedeor@users.noreply.github.com>
2022-11-23 16:33:21 -0800

    Add files via upload

--- code/rachel/Django/chirp_blog_lab03/posts/models.py
+++ code/rachel/Django/chirp_blog_lab03/posts/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=20)
     author = models.ForeignKey('auth.User', related_name="posts", on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
-    created_time = models.DateTimeField(auto_now=True)
+    edited_date = models.DateTimeField(auto_now=True)
     content = models.TextField(max_length=200)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("posts:detail", args=(self.pk,))
+    
+    class Meta:
+        ordering = ['-created_date']
