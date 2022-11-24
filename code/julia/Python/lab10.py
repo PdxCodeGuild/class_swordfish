98ad0a3491398a0f45eb604504ccf962b6aff91f
Julia Chobu <juliachobu@Julias-MacBook-Air.local>
2022-11-23 16:30:40 -0800

    Lab10 done, working on lab11.py

--- code/julia/Python/lab10.py
+++ code/julia/Python/lab10.py
@@ -1,14 +1,12 @@
 # Dad Joke API
 
 import requests
-import json
 import time 
-
-time.sleep(3)
 
 response = requests.get('https://icanhazdadjoke.com/', headers ={'accept' : 'Application/json'})
 
-def dadjoke(response):
-    if response == 200:
-        
+data = response.json()
 
+print("Joke loading...")
+time.sleep(3)
+print(f"\nResults: {data['joke']}")
