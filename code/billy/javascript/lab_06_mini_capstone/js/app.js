617f803fc543535bf69d4ad74c4917c4ceadd159
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-11-28 15:13:21 -0800

    Pushed changes for Django Lab 4 (ready for review) and changes for JS Lab 6 (incomplete).

--- code/billy/javascript/lab_06_mini_capstone/js/app.js
+++ code/billy/javascript/lab_06_mini_capstone/js/app.js
@@ -8,18 +8,32 @@ const vm = new Vue({
         currentType: '',
 
         responseData: {},
+
+        description: '',
+        name: '',
+        location: '',
+
+        searchTerm: '',
+
     },
     methods: {
         getAllData: function() {
             axios({
                 method: 'GET',
                 url: 'https://botw-compendium.herokuapp.com/api/v2/all',
+                // url: 'https://botw-compendium.herokuapp.com/api/v2/entry/',
                 params: {
-                    filter: this.currentType,
-                    term: this.currentName,
+                    // filter: this.currentType,
+                    // term: this.currentName,
+                    term: this.searchTerm,
                 }
         }).then((response)=> {
         this.responseData = response.data
+        for(let i = 0; i < 36; i++){
+            console.log(i)
+            console.log(this.description = response.data.data.creatures.food[i].name)
+        }
+        // console.log(this.description)
         })
     },
 },
