0155de9c2e422ce2f22fdfa6f951d3faff099a8b
ktabusa laptop <ktabusa@gmail.com>
2022-11-30 09:08:56 -0500

    updated JS Lab02 A Unit Converter to work with alerts

--- code/keenan/javascript/lab_02/unitconverter.js
+++ code/keenan/javascript/lab_02/unitconverter.js
@@ -1,41 +1,69 @@
 // code for a prompt based measurement converter
 
-let input = prompt('Enter the number of meters to convert: ')
-let unit_out = prompt('Please provide the conversion unit in its abbreviated form. What is the output unit? ')
+alert('Please provide all units of measure in abbreviated form.')
+
+let input = prompt('Enter a distance  to convert: ')
+let unit_in = prompt('Provide the units for the intial distance to convert: ')
+let unit_out = prompt('What unit would you like to convert this distance to? ')
 let output
-
-let unit_in
-
+let meters
 
 // can probably consolidate my rounding logic at the same time i fix that last double error
 // if output units is meters, run main section. if output is any of the others, convert m again to output
 
-if (unit_out === 'ft') {
-    output = input / 0.3048
+
+if (unit_in === 'ft') {
+    meters = input * 0.3048
+}
+else if (unit_in === 'mi') {
+    meters = input * 1609.34
+}
+else if (unit_in === 'm') {
+    meters = input
+}
+else if (unit_in === 'km') {
+    meters = input /  1000
+}
+else if (unit_in === 'yd') {
+    meters = input * 0.9144
+}
+else if (unit_in === 'in') {
+    meters = input * 0.0254
+}
+else {
+    alert('Enter a correct unit for the input distance')
+}
+
+// alert(meters + " meters")
+
+if (unit_out === 'm') {
+    output = meters
+} 
+else if (unit_out === 'ft') {
+    output = meters / 0.3048
     output = output.toFixed(2)
 }
 else if (unit_out === 'mi') {
-    output = input / 1609.34
+    output = meters / 1609.34
     output = output.toFixed(2)
 }
-else if (unit_out === 'm') {
-    output = input
-} 
+
 else if (unit_out === 'km') {
-    output = input / 1000
+    output = meters / 1000
     output = output.toFixed(2)
 }
 else if (unit_out === 'yd') {
-    output = input / 0.9144
+    output = meters / 0.9144
     output = output.toFixed(2)
 }
 else if (unit_out === 'in') {
-    output = input / 0.0254
+    output = meters / 0.0254
     output = output.toFixed(2)
 }
 else {
     alert('enter a correct unit')
 }
 
+
 // getting an undefined error because this is running after the 'else' statement
-alert(input + " meters is " + output + ' ' + unit_out)
+alert(input + ' ' + unit_in+ ' ' + output + ' ' + unit_out)
