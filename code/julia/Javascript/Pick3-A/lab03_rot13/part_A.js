716f3081d4a9ee6cd1a6ba41a32a4f2bd3e05960
Julia Chobu <juliachobu@Julias-MacBook-Air.local>
2022-11-29 18:08:43 -0800

    lab03_rot13 part A done

--- code/julia/Javascript/Pick3-A/lab03_rot13/part_A.js
+++ code/julia/Javascript/Pick3-A/lab03_rot13/part_A.js
@@ -1,38 +1,40 @@
 // Rot13
-
-
 let rot13={
-    "a" : n,
-    "b" : o,
-    "c" : p,
-    "d" : q,
-    "e" : r,
-    "f" : s,
-    "g" : t,
-    "h" : u,
-    "i" : v,
-    "j" : w,
-    "k" : x,
-    "l" : y,
-    "m" : z,
-    "n" : a,
-    "o" : b,
-    "p" : c,
-    "q" : d,
-    "r" : e,
-    "s" : f,
-    "t" : g,
-    "u" : h,
-    "v" : i,
-    "w" : j,
-    "x" : k,
-    "y" : l,
-    "z" : m,
+    "a" : "n",
+    "b" : "o",
+    "c" : "p",
+    "d" : "q",
+    "e" : "r",
+    "f" : "s",
+    "g" : "t",
+    "h" : "u",
+    "i" : "v",
+    "j" : "w",
+    "k" : "x",
+    "l" : "y",
+    "m" : "z",
+    "n" : "a",
+    "o" : "b",
+    "p" : "c",
+    "q" : "d",
+    "r" : "e",
+    "s" : "f",
+    "t" : "g",
+    "u" : "h",
+    "v" : "i",
+    "w" : "j",
+    "x" : "k",
+    "y" : "l",
+    "z" : "m",
 }
 
+let test = 'cat'
 
+let user_word = prompt("Type a word: ")
+let encoded_str = ''
 
+for (let char of user_word){
+    encoded_str += rot13[char]
+}
 
-
-user_word = prompt("Type a word: ")
-alert(rot13(user_word))
+alert(encoded_str)
