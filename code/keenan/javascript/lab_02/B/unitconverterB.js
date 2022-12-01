36bc2da8d140218b3cc68b00953c682ee4ad7d06
ktabusa laptop <ktabusa@gmail.com>
2022-12-01 12:44:38 -0500

    got the final have the index template set for final JS version B, finalizing functionality in the convertBtn function

--- code/keenan/javascript/lab_02/B/unitconverterB.js
+++ code/keenan/javascript/lab_02/B/unitconverterB.js
@@ -1,3 +1,9 @@
 let input = document.getElementById('input')
 let unit_in = document.getElementById('unit_in')
 let unit_out = document.getElementById('unit_out')
+
+
+convertBtn.addEventListener('click', function () {
+    // console.log(unit_in.value)
+    // console.log(parseFloat(input.value) * 1000)
+})
