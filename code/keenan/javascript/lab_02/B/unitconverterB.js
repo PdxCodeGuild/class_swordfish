d519db962651874975c3defbcfe18074d71d8ddc
ktabusa laptop <ktabusa@gmail.com>
2022-12-01 12:59:32 -0500

    Completed the final functionality for the JS Lab02 Unit Converter version B.

--- code/keenan/javascript/lab_02/B/unitconverterB.js
+++ code/keenan/javascript/lab_02/B/unitconverterB.js
@@ -2,8 +2,21 @@ let input = document.getElementById('input')
 let unit_in = document.getElementById('unit_in')
 let unit_out = document.getElementById('unit_out')
 
+const meters = {
+    mi:1609.34,
+    ft:0.3048,
+    m:1,
+    km:1000,
+    yd:0.9144,
+    inches:0.0254,
+}
 
 convertBtn.addEventListener('click', function () {
     // console.log(unit_in.value)
     // console.log(parseFloat(input.value) * 1000)
+    let output_distance = parseFloat(input.value) * meters[unit_in.value] / meters[unit_out.value]
+    // console.log(output_distance)
+    // console.log(output_distance.toFixed(2))
+    output_distance = output_distance.toFixed(2)
+    alert('Your output distance is ' + output_distance + ' ' + unit_out.value)
 })
