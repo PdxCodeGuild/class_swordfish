4a81df63fcc856509cfd68cb4e9244bfa64eb685
ktabusa laptop <ktabusa@gmail.com>
2022-12-02 18:07:51 -0500

    updating capstone with API readme notes

--- code/keenan/javascript/lab_06/js/capstone.js
+++ code/keenan/javascript/lab_06/js/capstone.js
@@ -5,12 +5,16 @@
 // You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:
 // https://api.nasa.gov/planetary/apod?api_key=S6bU3PcfNm6XpLCA8l4JoLYhYgnRM8ORcGbGFvmG
 
+
+// API readme and info: https://github.com/nasa/apod-api/blob/master/README.md
+
 const vm = new Vue({
     el: '#app',
     data: {
         nasaresponse: [],
-        date: '',
         hdur: '',
+        date: '',
+        concept_tags: '',
     },
     methods:{
         loadNasa: function() {
