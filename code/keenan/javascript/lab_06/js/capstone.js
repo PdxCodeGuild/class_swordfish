eddf40a9dbfa1967033060fd86614637a640a976
ktabusa laptop <ktabusa@gmail.com>
2022-12-02 18:54:15 -0500

    MVP for Javascript mini-capstone complete

--- code/keenan/javascript/lab_06/js/capstone.js
+++ code/keenan/javascript/lab_06/js/capstone.js
@@ -11,10 +11,11 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        nasaresponse: [],
+        nasaResponse: [],
         hdur: '',
         date: '',
-        concept_tags: '',
+        myDate: '',
+        // myConcept: '',
     },
     methods:{
         loadNasa: function() {
@@ -22,13 +23,40 @@ const vm = new Vue({
                 method: 'GET',
                 url: 'https://api.nasa.gov/planetary/apod?api_key=S6bU3PcfNm6XpLCA8l4JoLYhYgnRM8ORcGbGFvmG',
                 params: {
-            
+                    // concept_tags: 'True'
+                    // appears that concept_tag functionality is disabled currently?
                 },
             }).then(response => {
-                this.nasaresponse = response.data
+                this.nasaResponse = response.data
                 console.log(response.data)
             })
         },
+        searchDate: function() {
+            axios({
+                method: 'GET',
+                url: 'https://api.nasa.gov/planetary/apod?api_key=S6bU3PcfNm6XpLCA8l4JoLYhYgnRM8ORcGbGFvmG',
+                params: {
+                    // per docs - date is a string in YYYY-MM-DD
+                    date: this.myDate
+                },
+            }).then(response => {
+                this.nasaResponse = response.data
+                console.log(response.data)
+            })
+        },
+        // searchConcept: function() {
+        //     axios({
+        //         method: 'GET',
+        //         url: 'https://api.nasa.gov/planetary/apod?api_key=S6bU3PcfNm6XpLCA8l4JoLYhYgnRM8ORcGbGFvmG',
+        //         params: {
+
+
+        //         },
+        //     }).then(response => {
+        //         this.nasaresponse = response.data
+        //         console.log(response.data)
+        //     })
+        // }
     },
     beforeMount: function() {
         this.loadNasa()
