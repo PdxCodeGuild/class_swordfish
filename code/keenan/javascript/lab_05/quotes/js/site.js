53eaaccb50f8467355bfbe365c832b0a5d695453
ktabusa laptop <ktabusa@gmail.com>
2022-12-01 19:34:52 -0500

    updated quotes lab with keyword search functionality and component display functions

--- code/keenan/javascript/lab_05/quotes/js/site.js
+++ code/keenan/javascript/lab_05/quotes/js/site.js
@@ -1,23 +1,49 @@
+Vue.component('quote-component', {
+    template: `
+    <div>
+        <ul>
+            <li v-for="quote in quotes">{{quote.body}} By: {{quote.author}}<hr></li>
+        </ul>
+    </div>
+    `,
+    props: ['quotes'],
+})
+// can use a header with the searchTerm as a prop for the header in the template
+
 const vm = new Vue({
 	el: '#app',
 	data: {
-        message: 'Vue Connected',
-		quotes: {},
-
+        // message: 'Vue Connected',
+		quotes: [],
+        searchTerm: '',
 	},
 	methods:{
         loadQuotes: function() {
             axios({
-                method: 'get',
+                method: 'GET',
                 url: 'https://favqs.com/api/quotes',
                 headers: {
                     "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
-                }
+                },
             }).then(response => 
-				// console.log(response.data))
-                this.quotes = response.data)
-
+				// console.log(response.data)) -- this uses the entire api response
+                this.quotes = response.data.quotes)
          },
+         searchKey: function() {
+            axios({
+                methods: 'GET',
+                url: 'https://favqs.com/api/quotes',
+                headers: {
+                    "Authorization": `Token token="855df50978dc9afd6bf86579913c9f8b"`
+                },
+                params: {
+                    filter: this.searchTerm
+                }
+            }).then((response) => {
+                this.quotes = response.data.quotes
+                // console.log(response.data)
+            })
+         }
 	},
 	computed:{
 
