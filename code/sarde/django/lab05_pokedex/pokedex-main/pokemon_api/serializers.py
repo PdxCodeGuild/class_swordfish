ff18daae0e24373e31450d08b7ccedd5df1f019e
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2022-12-07 17:19:46 -0500

    lab05_pokedex in progress

--- code/sarde/django/lab05_pokedex/pokedex-main/pokemon_api/serializers.py
+++ code/sarde/django/lab05_pokedex/pokedex-main/pokemon_api/serializers.py
@@ -2,12 +2,19 @@ from rest_framework import serializers
 from pokemon.models import Pokemon
 from pokemon.models import Type
 
-class PokemonSerializer(serializers.ModelSerializer):
-    types = serializers.StringRelatedField(many=True)
 
+class NestedTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = (
+            'type',
+        )
+class PokemonSerializer(serializers.ModelSerializer):
+    # types = serializers.StringRelatedField(many=True)
+    type_detail = NestedTypeSerializer(read_only=True, many=True, source='types')
 
     class Meta:
-        fields = ('number', 'name', 'height', 'weight', 'image_front', 'image_back', 'caught_by', 'types')
+        fields = ('number', 'name', 'height', 'weight', 'image_front', 'image_back', 'type_detail')
         model = Pokemon
 # For each pokemon object, types should be fetched from database
 the_type = Pokemon.objects.all()
