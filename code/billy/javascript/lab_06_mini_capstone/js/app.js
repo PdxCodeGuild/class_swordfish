fe4700e330248fdc37a55897f3a38b482f18b91b
GirthyGnome <112988489+GirthyGnome@users.noreply.github.com>
2022-12-07 16:42:23 -0800

    billy frick - pushed additional changes to JavaScript Capstone. The project is almost complete, and my course capstone can be found on my personal github repo (changes were pushed there).

--- code/billy/javascript/lab_06_mini_capstone/js/app.js
+++ code/billy/javascript/lab_06_mini_capstone/js/app.js
@@ -1,46 +1,19 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        inputName: '',
-        inputType: '',
-
-        currentName: '',
-        currentType: '',
-
         responseData: {},
 
-        description: '',
-        name: '',
-        location: '',
-
         searchTerm: '',
-
     },
     methods: {
-        getAllData: function() {
+        getGameInformation: function() {
             axios({
                 method: 'GET',
-                url: 'https://botw-compendium.herokuapp.com/api/v2/all',
-                // url: 'https://botw-compendium.herokuapp.com/api/v2/entry/',
-                params: {
-                    // filter: this.currentType,
-                    // term: this.currentName,
-                    term: this.searchTerm,
-                }
+                url: `https://botw-compendium.herokuapp.com/api/v2/entry/${this.searchTerm}`,
         }).then((response)=> {
-        this.responseData = response.data
-        for(let i = 0; i < 36; i++){
-            console.log(i)
-            console.log(this.description = response.data.data.creatures.food[i].name)
+            this.responseData = response.data
+            console.log(this.responseData)
         }
-        // console.log(this.description)
-        })
-    },
+    )},
 },
-    computed: {
-    },
-    beforeMount: function() {
-        this.getAllData()
-        console.log(this.getAllData)
-    }
 })
