4278093697d5a7b4f101be469157e67b46b9d688
Julia Chobu <juliachobu@Julias-MacBook-Air.local>
2022-12-09 08:54:25 -0800

    working on different labs

---
+++ code/julia/Javascript/todos/js/site.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+        todos:[
+            { id: 0, text: 'Complete your homework'},
+            { id: 1, text: 'Go grocery shopping'},
+            { id: 2, text: 'Finish laundry'},
+            { id: 3, text: 'Make dinner'},
+        ],
+        nextTodoId: 5
+    },
+
+    methods:{
+        addTodo: function(){
+           let newTodo = this.todos
+    }
+})
