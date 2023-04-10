34703d512c7b6aa443c815d9105fadcb90ab080c
Sardé Essilfie <86763001+essineryan@users.noreply.github.com>
2023-04-10 15:53:32 -0400

    unit converter redo in progress

---
+++ code/sarde/python/practice/lab01(redo).py
@@ -0,0 +1,17 @@
+'''
+#Lab 1: Unit Converter(redo)
+'''
+'''
+This lab will involve writing a program, that allows the user to convert a number between units
+'''
+'''
+Version 1
+'''
+'''
+Ask the user for the number of feet, and print out the equivalent distance in meters. 
+Hint: 1 ft is 0.3048 m. 
+So we can get the output in meters by multiplying the input distance by 0.3048.
+'''
+units = {
+    'feet': 0.3048,
+}
